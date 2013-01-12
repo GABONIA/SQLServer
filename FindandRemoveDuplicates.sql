93d0e9d32fdf1c5cf97612c0385ad75a8e5e3545
Tim <tmmtsmith@gmail.com>
2013-01-11 20:00:27 -0600

    Included example

--- FindandRemoveDuplicates.sql
+++ FindandRemoveDuplicates.sql
@@ -28,3 +28,38 @@ AS
 FROM OurTableName)
 DELETE FROM DuplicateRemover
 WHERE DupCounter > 1
+
+/* EXAMPLE */
+
+-- Creates a temp table 
+DECLARE @clean TABLE (
+	A INT, 
+	B INT, 
+	Date DATETIME
+)
+
+-- Insert values into the temp table
+INSERT INTO @clean VALUES(1,1,getdate())
+INSERT INTO @clean VALUES(1,1,DATEADD(DD,-2,getdate()))
+INSERT INTO @clean VALUES(1,1,DATEADD(DD,-5,getdate()))
+INSERT INTO @clean VALUES(1,2,DATEADD(DD,-10,getdate()))
+INSERT INTO @clean VALUES(1,2,DATEADD(DD,-15,getdate()))
+INSERT INTO @clean VALUES(3,1,getdate())
+
+-- Shows the full amount of values in the temp table
+SELECT *
+FROM @clean
+
+-- Deletes the duplicate B's (notice that there were 3 1s and 2 2s in the B column)
+;WITH DuplicateRemover (A,B,DupCounter)
+AS
+(SELECT A
+,B
+,ROW_NUMBER() OVER(PARTITION BY A,B ORDER BY Date DESC) AS DupCounter
+FROM @clean)
+DELETE FROM DuplicateRemover
+WHERE DupCounter > 1
+
+-- Temp table without the duplicates
+SELECT *
+FROM @clean
