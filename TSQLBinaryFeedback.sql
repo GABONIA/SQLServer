c8bfa395a8071618eaca56c87ab8a7ee678b19c5
Tim <tmmtsmith@gmail.com>
2013-01-20 08:54:54 -0600

    Binary feedback example
    Provides an example of how to have a binary feedback model; for
    instance, if you had a like and dislike button, the database in this
    example would count everything (this allows for time-tracking).  The
    other example would be a simple update to increment likes or dislikes.

---
+++ TSQLBinaryFeedback.sql
@@ -0,0 +1,31 @@
+/* 
+
+Back end design example for binary feedback, which allows tracking of feedback over time
+
+*/
+-- Build the experiment table
+DECLARE @meas TABLE(
+	Positive INT,
+	Negative INT,
+	Date DATETIME
+)
+-- Insert test values into the experiment table
+INSERT INTO @meas (Positive,Date) VALUES (1,GETDATE())
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-1,GETDATE()))
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-2,GETDATE()))
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-7,GETDATE()))
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-13,GETDATE()))
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-27,GETDATE()))
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-35,GETDATE()))
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-41,GETDATE()))
+INSERT INTO @meas (Positive,Date) VALUES (1,DATEADD(DD,-72,GETDATE()))
+INSERT INTO @meas (Negative,Date) VALUES (1,DATEADD(DD,-2,GETDATE()))
+INSERT INTO @meas (Negative,Date) VALUES (1,DATEADD(DD,-12,GETDATE()))
+INSERT INTO @meas (Negative,Date) VALUES (1,DATEADD(DD,-36,GETDATE()))
+-- Shows us our total positive and negative scores
+SELECT SUM(Positive) AS Positive, SUM(Negative) AS Negative
+FROM @meas
+-- Shows us our positive and negative scores for the last month
+SELECT SUM(Positive) AS Positive, SUM(Negative) AS Negative
+FROM @meas
+WHERE Date > DATEADD(DD,-31,GETDATE())
