21f66fa5dd3dd1ea965922b18eef020a6e583c12
Tim <tmmtsmith@gmail.com>
2013-01-22 09:16:48 -0600

    Updated

--- NonParameterDMVsDMFs.sql
+++ NonParameterDMVsDMFs.sql
@@ -26,24 +26,24 @@ FROM sys.dm_exec_query_stats
 
 SELECT *
 FROM sys.dm_db_index_usage_stats
---
+-- Returns index related information including when it happened
 
 SELECT *
 FROM sys.dm_os_performance_counters
---
+-- Provides performance counters
 
 SELECT *
 FROM sys.dm_os_schedulers
---
+-- Helpful for reviewing schedulers
 
 SELECT *
 FROM sys.dm_os_nodes
---
+-- Gives information about nodes, imitating hardware process locality
 
 SELECT *
 FROM sys.dm_os_waiting_tasks
---
+-- Provides information about tasks waiting
 
 SELECT *
 FROM sys.dm_os_wait_stats
---
+-- Good for assessing performance issues with the server
