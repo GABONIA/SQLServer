e0827a0c1c3b0a1dc9951d576f534bd17223e96b
Tim <tmmtsmith@gmail.com>
2013-01-23 10:49:29 -0600

    TSQL Coalesce examples

--- CoalesceExamples.sql
+++ CoalesceExamples.sql
@@ -26,14 +26,14 @@ FROM @null
 SELECT COALESCE(ColumnOne,ColumnTwo,ColumnThree,ColumnFour) AS Grade
 FROM @null
 
--- Build test table
+-- Build test table for returning preferred method of phone contact
 DECLARE @phone TABLE(
 	Home VARCHAR(15),
 	Business VARCHAR(15),
 	Cell VARCHAR(15)
 )
 
--- Insert test values
+-- Insert phone values
 INSERT INTO @phone VALUES ('8005551212',NULL,NULL)
 INSERT INTO @phone VALUES (NULL,'8005552121',NULL)
 INSERT INTO @phone VALUES (NULL,NULL,'8005551122')
@@ -41,10 +41,10 @@ INSERT INTO @phone VALUES (NULL,'8005552121','8005551122')
 INSERT INTO @phone VALUES ('8005551212','8005552121',NULL)
 INSERT INTO @phone VALUES ('8005551212','8005552121','8005551122')
 
--- Select from test values
+-- Select from phone values
 SELECT *
 FROM @phone
 
--- Test COALESCE
+-- Business Logic: first Home, then Business, finally Cell
 SELECT COALESCE(Home,Business,Cell) AS "Preferred Phone"
 FROM @phone
