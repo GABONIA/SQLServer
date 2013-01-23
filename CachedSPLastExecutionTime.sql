1e4be762fcbd1fa62e3114f728da5f919fc6d7b4
Tim <tmmtsmith@gmail.com>
2013-01-23 16:05:38 -0600

    Last Execution Time of Cached Store Procedure
    Added finding and object's id to this file.

--- CachedSPLastExecutionTime.sql
+++ CachedSPLastExecutionTime.sql
@@ -8,3 +8,14 @@ SELECT SO.name, SD.last_execution_time
 FROM sys.dm_exec_procedure_stats SD
     INNER JOIN sys.objects SO ON SO.object_id = SD.object_id
 WHERE SO.name = 'usp_InsertOurProcName'
+
+
+/* 
+
+Find an object's id
+
+*/
+
+SELECT OBJECT_ID('OurDatabase..OurTable')
+SELECT OBJECT_ID('OurDatabase..OurStoredProcedure')
+
