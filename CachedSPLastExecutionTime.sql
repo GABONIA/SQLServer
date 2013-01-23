b55d57c250cfd0e06185587c811d3f8dfc6821f7
Tim <tmmtsmith@gmail.com>
2013-01-23 13:59:54 -0600

    Last Execution Time of a Non-Cached Procedure

---
+++ CachedSPLastExecutionTime.sql
@@ -0,0 +1,10 @@
+/* 
+
+Finds the last time a stored procedure was run, assuming the cache has not been cleared.
+
+*/
+
+SELECT SO.name, SD.last_execution_time
+FROM sys.dm_exec_procedure_stats SD
+    INNER JOIN sys.objects SO ON SO.object_id = SD.object_id
+WHERE SO.name = 'usp_InsertOurProcName'
