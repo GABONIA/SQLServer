0d3ad34569e333d47c22d6b8912f48f2a5e4740f
tmmtsmith' <tmmtsmith@gmail.com>
2013-02-01 08:17:32 -0600

    Updated

--- TSQLMode.sql
+++ TSQLMode.sql
@@ -4,7 +4,7 @@ Calculates the mode of a data set
 
 */
 
-SELECT TOP 1 Value, COUNT(*) Mode
-FROM research
-GROUP BY Value
+SELECT TOP 1 OurValue, COUNT(*) Mode
+FROM OurTable
+GROUP BY OurValue
 ORDER BY COUNT(*) DESC
