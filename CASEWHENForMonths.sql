380fe86935dd7d9846a542645872ba54f6bcf285
tmmtsmith <tmmtsmith@gmail.com>
2013-02-07 08:54:43 -0600

    Months to digits (long and short months)

--- CASEWHENForMonths.sql
+++ CASEWHENForMonths.sql
@@ -41,6 +41,39 @@ CASE
 	ELSE 99 -- This will be removed
 END
 
+-- Version 3: Short months 1 digits months
+CASE 
+	WHEN [OurColumnName] LIKE '%Jan%' THEN 1 
+	WHEN [OurColumnName] LIKE '%Feb%' THEN 2
+	WHEN [OurColumnName] LIKE '%Mar%' THEN 3
+	WHEN [OurColumnName] LIKE '%April%' THEN 4
+	WHEN [OurColumnName] LIKE '%May%' THEN 5
+	WHEN [OurColumnName] LIKE '%June%' THEN 6
+	WHEN [OurColumnName] LIKE '%July%' THEN 7
+	WHEN [OurColumnName] LIKE '%Aug%' THEN 8
+	WHEN [OurColumnName] LIKE '%Sep%' THEN 9
+	WHEN [OurColumnName] LIKE '%Oct%' THEN 10
+	WHEN [OurColumnName] LIKE '%Nov%' THEN 11
+	WHEN [OurColumnName] LIKE '%Dec%' THEN 12
+	ELSE 99 -- This will be removed
+END
+
+-- Version 4: Short months 2 digits months
+CASE 
+	WHEN [OurColumnName] LIKE '%Jan%' THEN 01 
+	WHEN [OurColumnName] LIKE '%Feb%' THEN 02
+	WHEN [OurColumnName] LIKE '%Mar%' THEN 03
+	WHEN [OurColumnName] LIKE '%April%' THEN 04
+	WHEN [OurColumnName] LIKE '%May%' THEN 05
+	WHEN [OurColumnName] LIKE '%June%' THEN 06
+	WHEN [OurColumnName] LIKE '%July%' THEN 07
+	WHEN [OurColumnName] LIKE '%Aug%' THEN 08
+	WHEN [OurColumnName] LIKE '%Sep%' THEN 09
+	WHEN [OurColumnName] LIKE '%Oct%' THEN 10
+	WHEN [OurColumnName] LIKE '%Nov%' THEN 11
+	WHEN [OurColumnName] LIKE '%Dec%' THEN 12
+	ELSE 99 -- This will be removed
+END
 
 /* EXAMPLE: */
 
