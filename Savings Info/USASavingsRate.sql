0a626a70ce8c47e8f054d414a9085dbfb6531b28
Tim <tmmtsmith@gmail.com>
2013-03-07 22:09:21 -0600

    Ranking top savings' years

--- Savings Info/USASavingsRate.sql
+++ Savings Info/USASavingsRate.sql
@@ -55,6 +55,16 @@ SELECT MIN(Date) "Year", AVG(Rate) "Savings Rate"
 FROM @save
 GROUP BY YEAR(Date)
 
+DECLARE @top TABLE(
+	[Year] INT,
+	[Savings Rate Percent Year Over Year] DECIMAL(7,4)
+)
+
+INSERT INTO @top
 SELECT YEAR(t2.Date) "Year", (((t2.Rate - t1.Rate)/t1.Rate)*100) "Savings Rate Increase/Decrease Year Over Year"
 FROM savepercent t1, savepercent t2
 WHERE t1.SaveID = t2.SaveID - 1
+
+SELECT *
+FROM @top
+ORDER BY [Savings Rate Percent Year Over Year] DESC
