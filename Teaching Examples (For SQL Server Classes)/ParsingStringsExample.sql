a2b6245e86745cb64f79cd019ed29b92c4c8051e
Tim <tmmtsmith@gmail.com>
2013-03-19 07:58:10 -0500

    Grabs the string between commas
    Also peforms calculation of how many spaces between values

--- Teaching Examples (For SQL Server Classes)/ParsingStringsExample.sql
+++ Teaching Examples (For SQL Server Classes)/ParsingStringsExample.sql
@@ -1,20 +1,20 @@
 /* 
 
-Parsing values out of a string
+Example: Parsing values out of a string
 
 */
 
 DECLARE @comma VARCHAR(75)
-SET @comma = 'hello,my,name,is,comma,and,i,am,unable,to,say,separate'
+SET @comma = 'hello,my,name,is,comma,and,i,am,unable,to,say,separate,'
 DECLARE @begin INT = 0
 DECLARE @count INT = 0
 
 WHILE @count < 12
 BEGIN
-	-- USE SUBSTRING
+	-- Parses every string except "separate" unless comma follows it
+	SELECT REPLACE(SUBSTRING(@comma,@begin,CHARINDEX(',',@comma,@begin+1)-@begin),',','')
 	SELECT CHARINDEX(',',@comma,@begin+1)
 	SET @begin = CHARINDEX(',',@comma,@begin+1)
 	
 	SET @count = @count + 1
 END
-
