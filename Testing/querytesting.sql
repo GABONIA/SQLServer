05404d5c217cf4c1ac63bcbbf4bb7180a81110e0
Tim <tmmtsmith@gmail.com>
2013-08-05 11:54:02 -0500

    Query performance testing

---
+++ Testing/querytesting.sql
@@ -0,0 +1,31 @@
+/*
+
+Query Performance Testing
+
+*/
+
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+
+
+PRINT '
+Starting test.  Testing query one ...'
+
+-- Query one:
+
+
+PRINT '
+Query one completed.'
+
+PRINT '
+Starting test.  Testing query two ...'
+
+-- Query two:
+
+
+PRINT '
+Query two completed.'
+
+
+SET STATISTICS IO OFF
+SET STATISTICS TIME OFF
