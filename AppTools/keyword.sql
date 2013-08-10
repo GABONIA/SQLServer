956b5eea807c33613389c336b78a4dc375b5a402
Tim <tmmtsmith@gmail.com>
2013-08-10 07:09:56 -0500

    Keyword replacement tool - limited string update

--- AppTools/keyword.sql
+++ AppTools/keyword.sql
@@ -1,16 +1,14 @@
+/*
 
-/* 
-
-Keyword replace tool
+Keyword parsing tool
 
 */
-
 DECLARE @string TABLE(
   String VARCHAR(MAX)
 )
 
 INSERT INTO @string
-VALUES ('The quick brown fox jumped over the lazy dogs.  The dogs heard this often written idiom and contacted Ruff, their attorney.  They filed a lawsuit against all the writers and developers using this often quoted idiom.')
+VALUES ('The quick brown fox jumped over the lazy dogs.  Each dog heard this often written idiom and contacted Ruff, their attorney.  They filed a lawsuit against all the writers and developers using this often quoted idiom.  Unfortunately, Ruff did not win this case, as the judge thought that the idiom was just that - an idiom.  He frowned upon the actions of the dogs.')
 
 DECLARE @keyword TABLE(
 	ID SMALLINT IDENTITY(1,1),
@@ -34,9 +32,15 @@ BEGIN
 	SELECT @key = Keyword FROM @keyword WHERE ID = @begin
 	SELECT @link = Link FROM @keyword WHERE ID = @begin
 
+	-- Limited string update
 	UPDATE @string
-	SET String = REPLACE(String,@key,@link)
-	FROM @String
+	SET String = REPLACE(SUBSTRING(String,1,150),@key,@link) + SUBSTRING(String,151,8000)
+	FROM @string
+
+	-- Unlimited string update
+	--UPDATE @string
+	--SET String = REPLACE(String,@key,@link)
+	--FROM @string
 
 	SET @begin = @begin + 1
 	SET @key = NULL
