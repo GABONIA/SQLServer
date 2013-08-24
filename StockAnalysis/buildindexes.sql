d941ce937ea4a0e239ca62911663c05035ed9b6b
Tim <tmmtsmith@gmail.com>
2013-08-24 05:25:05 -0500

    Build index procedure

---
+++ StockAnalysis/buildindexes.sql
@@ -0,0 +1,50 @@
+USE [StockAnalysis]
+GO
+
+/****** Object:  StoredProcedure [dbo].[stp_BuildIndexes]    Script Date: 8/24/2013 5:24:33 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[stp_BuildIndexes]
+AS
+BEGIN
+
+	DECLARE @begin INT = 1
+	DECLARE @max INT
+	DECLARE @symbol VARCHAR(10)
+	SELECT @max = MAX(ID) FROM AddedStockTable
+
+	WHILE @begin <= @max
+	BEGIN
+		
+		SELECT @symbol = StockSymbol FROM AddedStockTable WHERE ID = @begin
+
+		DECLARE @sql NVARCHAR(MAX)
+		SET @sql = 'CREATE CLUSTERED INDEX [Date] ON [dbo].[' + @symbol + 'HistoricalData]
+		(
+			[Date] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+
+		PRINT ''Clustered index created''
+
+		CREATE NONCLUSTERED INDEX [' + @symbol + 'ID] ON [dbo].[' + @symbol + 'HistoricalData]
+		(
+			[' + @symbol + 'ID] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+
+		PRINT ''Non clustered index created'''
+
+		EXECUTE(@sql)
+
+		SET @begin = @begin + 1
+		SET @sql = ''
+
+	END
+
+END
+GO
+
+
