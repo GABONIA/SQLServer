f246aab49de4412fd5bcf94f5ed50b474b3e3927
Tim <tmmtsmith@gmail.com>
2013-09-06 06:38:37 -0500

    Month procedures

--- StockAnalysis/month.sql
+++ StockAnalysis/month.sql
@@ -2,7 +2,8 @@ USE StockAnalysis
 GO
 
 
-ALTER PROCEDURE stp_MonthToMonth
+-- Ranking for each month
+CREATE PROCEDURE stp_MonthToMonth
 @sym VARCHAR(10)
 AS
 BEGIN
@@ -26,4 +27,32 @@ BEGIN
 
 	EXECUTE sp_executesql @sql
 
+END
+
+
+-- All months listed and ranked
+CREATE PROCEDURE stp_IndividualMonth
+@sym VARCHAR(10)
+AS
+BEGIN
+
+	SET @sym = UPPER(@sym)
+	
+	DECLARE @sql NVARCHAR(MAX)
+
+	SET @sql = ';WITH CTE AS(
+		SELECT DATENAME(MONTH, s2.Date) EachMonth
+			, YEAR(s2.Date) EachYear
+			, (((s2.Price - s.Price)/s.Price)*100) MonthGrowth
+		FROM ' + @sym + 'HistoricalData s
+			INNER JOIN ' + @sym + 'HistoricalData s2 ON s.Date = DATEADD(MM,-1,s2.Date)
+		WHERE DAY(s.Date) = 1
+	)
+	SELECT EachMonth + '' '' + CAST(EachYear AS VARCHAR(4))
+		, MonthGrowth
+	FROM CTE
+	ORDER BY MonthGrowth DESC'
+
+
+	EXECUTE sp_executesql @sql
 END
