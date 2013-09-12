ccaa96b82db62245cce57e98f03870cce95b34da
Tim <tmmtsmith@gmail.com>
2013-09-12 14:53:17 -0500

    Server side trace

--- Admin Tools/logintrace.sql
+++ Admin Tools/logintrace.sql
@@ -42,20 +42,20 @@ IF (@rc != 0) GOTO error
 
 /* Events */
 -- Auditing Logins (14), Logouts (15), Existing Connections (17)
--- NTUserName (6), NTDomainName (7), ApplicationName (10), SPID (12)
+-- NTUserName (6), LoginName (11), ApplicationName (10), SPID (12)
 
 DECLARE @on BIT
 SET @on = 1
 EXEC sp_trace_setevent @TraceID, 14, 6, @on
-EXEC sp_trace_setevent @TraceID, 14, 7, @on
+EXEC sp_trace_setevent @TraceID, 14, 11, @on
 EXEC sp_trace_setevent @TraceID, 14, 10, @on
 EXEC sp_trace_setevent @TraceID, 14, 12, @on
 EXEC sp_trace_setevent @TraceID, 15, 6, @on
-EXEC sp_trace_setevent @TraceID, 15, 7, @on
+EXEC sp_trace_setevent @TraceID, 15, 11, @on
 EXEC sp_trace_setevent @TraceID, 15, 10, @on
 EXEC sp_trace_setevent @TraceID, 15, 12, @on
 EXEC sp_trace_setevent @TraceID, 17, 6, @on
-EXEC sp_trace_setevent @TraceID, 17, 7, @on
+EXEC sp_trace_setevent @TraceID, 17, 11, @on
 EXEC sp_trace_setevent @TraceID, 17, 10, @on
 EXEC sp_trace_setevent @TraceID, 17, 12, @on
 
