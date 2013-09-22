7f6f5ad3fa64792eff3e912c9a8be4307d1f7812
Tim <tmmtsmith@gmail.com>
2013-09-22 08:43:34 -0500

    Create testingapproach.sql

---
+++ StockAnalysis/testingapproach.sql
@@ -0,0 +1,46 @@
+/*
+
+Similar to a straight vs. M-Red approach (not exact, but does break down each)
+
+*/
+
+
+SET STATISTICS TIME ON
+
+
+SELECT m.StockSymbol
+	, a.Price
+FROM dbo.AllHistoricalData a
+	INNER JOIN MainStockTable m ON a.ID = m.ID
+WHERE m.StockSymbol = 'SO'
+	AND a.Date BETWEEN '2011-01-01' AND '2011-03-31' 
+
+
+PRINT '
+
+First test done
+
+'
+
+
+DECLARE @sql NVARCHAR(MAX), @sym VARCHAR(250), @begin DATE, @end DATE
+SET @sym = '' -- Any enter here
+SET @begin = '2011-01-01'
+SET @end = '2011-03-31'
+
+SET @sql = 'SELECT ''' + @sym + '''
+				, Price
+			FROM ' + @sym + 'HistoricalData
+			WHERE Date BETWEEN ''' + CAST(@begin AS VARCHAR) + ''' AND ''' + CAST(@end AS VARCHAR) + ''''
+
+EXECUTE(@sql)
+
+
+PRINT '
+
+Second test done
+
+'
+
+
+SET STATISTICS TIME OFF
