73136bdeba8c0d6de60ed60e6a6f08668aebc64d
Tim <tmmtsmith@gmail.com>
2013-09-24 06:08:05 -0500

    Outputs JSON formatted data (step one)

---
+++ StockAnalysis/JSONOutput.sql
@@ -0,0 +1,24 @@
+/*
+
+Stock schema tables only
+
+*/
+
+CREATE PROCEDURE stp_StockDataJSONOuput
+@sym VARCHAR(250)
+AS
+BEGIN
+
+	DECLARE @sql NVARCHAR(MAX)
+	-- Testing: DECLARE @sym VARCHAR(250) = ''
+
+	SET @sql = 'SELECT ''{"' + @sym + 'ID": "'' + CAST(' + @sym + 'ID AS VARCHAR) + ''",
+		"Date": "'' + CAST([Date] AS VARCHAR) + ''"
+		"Price": "'' + CAST(Price AS VARCHAR) + ''" 
+		"TwoHundredDaySMA": "'' + CAST(TwoHundredDaySMA AS VARCHAR) + ''"''
+	FROM stock.' + @sym + 'HistoricalData'
+
+	EXECUTE(@sql)
+
+
+END
