f4722faa7f5d188f734f2546f3052e10f89613e6
Tim <tmmtsmith@gmail.com>
2013-09-25 06:19:31 -0500

    Update JSONOutput.sql

--- StockAnalysis/JSONOutput.sql
+++ StockAnalysis/JSONOutput.sql
@@ -21,4 +21,45 @@ BEGIN
 	EXECUTE(@sql)
 
 
+/*
+
+CREATE TABLE ##JSONTemp(
+	ID INT IDENTITY(1,1),
+	JSONData VARCHAR(8000)
+)
+
+INSERT INTO ##JSONTemp (JSONData)
+EXECUTE stp_StockDataJSONOuput 'BAC'
+
+DECLARE @begin INT = 1, @max INT, @string VARCHAR(MAX) = '', @temp VARCHAR(MAX)
+SELECT @max = MAX(ID) FROM ##JSONTemp
+
+WHILE @begin <= @max
+BEGIN
+	
+	SELECT @temp = JSONData FROM ##JSONTemp WHERE ID = @begin
+	
+	IF @begin = @max
+	BEGIN
+	
+		SELECT @string = @string + @temp
+		
+	END
+	ELSE
+	BEGIN
+
+		SELECT @string = @string + @temp + ', '
+
+	END
+
+	SET @begin = @begin + 1
+	SET @temp = ''
+
+END
+
+SELECT @string
+
+*/
+
+
 END
