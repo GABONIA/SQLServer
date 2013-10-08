a4154e7f1ff22b2aba6b6a32b02d6403bd71bc33
Tim <tmmtsmith@gmail.com>
2013-10-08 05:12:55 -0500

    Updated administrative jobs

--- Admin Tools/AdministrativeJobs.sql
+++ Admin Tools/AdministrativeJobs.sql
@@ -1,100 +1,132 @@
-USE DatabaseName
-GO
-
-CREATE PROCEDURE AS stp_AdministrativeJobs
+ALTER PROCEDURE [admin].[stp_StockAnalysis_Admin]
 AS
 BEGIN
 
-/* Rebuild All Non-System Indexes - fails on multiple column tables, see below script */
+/* 
 
-DECLARE @RebuildIndex TABLE(
-  IndexingID INT IDENTITY(1,1),
-  IndexName VARCHAR(250),
-  TableSchema VARCHAR(50),
-  TableName VARCHAR(250)
-)
+Rebuild indexes for each table
 
-INSERT INTO @RebuildIndex (IndexName,TableSchema,TableName)
-SELECT i.name AS IndexName
-	, SCHEMA_NAME(schema_id) AS TableSchema
-	, o.name AS TableName
-FROM sys.indexes i
-	INNER JOIN sys.objects o ON i.object_id = o.object_id
-	INNER JOIN sys.index_columns c ON c.object_id = o.object_id AND i.index_id = c.index_id
-	INNER JOIN sys.columns sc ON sc.object_id = o.object_id AND sc.column_id = c.column_id
-WHERE o.is_ms_shipped = 0
+*/
 
-DECLARE @begin INT = 1
-DECLARE @max INT
-SELECT @max = MAX(IndexingID) FROM @RebuildIndex
-DECLARE @IndexName VARCHAR(250), @TableSchema VARCHAR(50), @TableName VARCHAR(250)
+-- This script assumes no data can be inserted between existind data; if this assumption is false, use coded out script below
+EXEC sp_msforeachtable 'ALTER INDEX ALL ON ? REBUILD PARTITION = ALL WITH ( FILLFACTOR  = 100, PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, ONLINE = OFF, SORT_IN_TEMPDB = OFF )'
 
-WHILE @begin <= @max
+-- Mid data inserts - fill factor can be 70 or 80 depending on how many mid-data inserts there are
+-- EXEC sp_msforeachtable 'ALTER INDEX ALL ON ? REBUILD PARTITION = ALL WITH ( FILLFACTOR  = 80, PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, ONLINE = OFF, SORT_IN_TEMPDB = OFF )'
+
+-- Check off item
+INSERT INTO admin.AdministrativeTasks
+SELECT 'Indexing Complete', GETDATE()
+
+/*
+
+Database integrity check and verify
+
+*/
+
+IF OBJECT_ID('##DatabaseConsistency') IS NOT NULL
 BEGIN
-
-	SELECT @IndexName = IndexName FROM @RebuildIndex WHERE IndexingID = @begin
-	SELECT @TableSchema = TableSchema FROM @RebuildIndex WHERE IndexingID = @begin
-	SELECT @TableName = TableName FROM @RebuildIndex WHERE IndexingID = @begin
-
-	DECLARE @sql NVARCHAR(MAX)
-	SET @sql = 'ALTER INDEX [' + @IndexName + '] ON [' + @TableSchema + '].[' + @TableName + '] REBUILD PARTITION = ALL WITH ( FILLFACTOR  = 80, PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, ONLINE = OFF, SORT_IN_TEMPDB = OFF )'
-
-	EXECUTE(@sql)
-
-	SET @begin = @begin + 1
-
+	DROP TABLE ##DatabaseConsistency
 END
 
--- COMPARE TO: sp_msforeachtable 'ALTER INDEX ALL ON ? REBUILD PARTITION = ALL WITH ( FILLFACTOR  = 80, PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, ONLINE = OFF, SORT_IN_TEMPDB = OFF )'
-
-/* Back up database */
-
-DECLARE @name VARCHAR(25)  
-DECLARE @path VARCHAR(256) 
-DECLARE @fileName VARCHAR(256)  
-DECLARE @fileDate VARCHAR(20)
-
-SET @name = 'DatabaseName'
-SET @path = 'D:\FilePath\'
-SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
-SET @fileName = @path + @name + '_' + @fileDate + '.BAK'
-BACKUP DATABASE @name TO DISK = @fileName
-
-/* Database Integrity Check, will return results from the temporary table */
-
-IF OBJECT_ID('##Consistency') IS NOT NULL
-BEGIN
-	DROP TABLE ##Consistency
-END
-
-CREATE TABLE ##Consistency(
-	Error VARCHAR(10),
-	Level VARCHAR(10),
-	State VARCHAR(10),
+CREATE TABLE ##DatabaseConsistency(
+	Error VARCHAR(10) NULL,
+	Level VARCHAR(10) NULL,
+	State VARCHAR(10) NULL,
 	MessageText VARCHAR(2000),
-	RepairLevel VARCHAR(250),
-	Status VARCHAR(10),
-	DbId VARCHAR(10),
-	ObjectID VARCHAR(25),
-	IndexID VARCHAR(10),
-	PartitionID VARCHAR(10),
-	AllocUnitId VARCHAR(10),
-	[File] VARCHAR(10),
-	Page VARCHAR(10),
-	Slot VARCHAR(10),
-	RefFile VARCHAR(10),
-	RefPage VARCHAR(10),
-	RefSlot VARCHAR(10),
-	Allocation VARCHAR(10)
+	RepairLevel VARCHAR(250) NULL,
+	Status VARCHAR(10) NULL,
+	DbId VARCHAR(10) NULL,
+	DbFragId VARCHAR(10) NULL,
+	ObjectID VARCHAR(25) NULL,
+	IndexID VARCHAR(10) NULL,
+	PartitionID VARCHAR(10) NULL,
+	AllocUnitId VARCHAR(10) NULL,
+	RidDbID VARCHAR(10) NULL,
+	RidPruId VARCHAR(10) NULL,
+	[File] VARCHAR(10) NULL,
+	Page VARCHAR(10) NULL,
+	Slot VARCHAR(10) NULL,
+	RefDbId VARCHAR(10) NULL,
+	RefPruId VARCHAR(10) NULL,
+	RefFile VARCHAR(10) NULL,
+	RefPage VARCHAR(10) NULL,
+	RefSlot VARCHAR(10) NULL,
+	Allocation VARCHAR(10) NULL
 )
 
-INSERT INTO ##Consistency
-EXEC('DBCC CHECKDB(DatabaseName) WITH TABLERESULTS')
+INSERT INTO ##DatabaseConsistency
+EXEC('DBCC CHECKDB(StockAnalysis) WITH TABLERESULTS')
 
-SELECT *
-FROM ##Consistency
-WHERE MessageText LIKE 'CHECKDB found%'
+INSERT INTO admin.IntegrityCheck (Error,Level,State,MessageText,RepairLevel,Status)
+SELECT Error, Level ,State, MessageText, RepairLevel, Status
+FROM ##DatabaseConsistency
 
-DROP TABLE ##Consistency
+-- Check off item
+INSERT INTO admin.AdministrativeTasks
+SELECT 'Database Integrity Checked', GETDATE()
+
+/* Backup the database if integrity exists */
+
+DECLARE @count TINYINT
+
+SELECT @count = COUNT(*)
+FROM ##DatabaseConsistency
+WHERE MessageText LIKE 'CHECKDB found 0 allocation errors and 0 consistency errors%'
+
+IF @count > 0
+BEGIN
+	
+	-- Back up database
+	DECLARE @name VARCHAR(25)  
+	DECLARE @path VARCHAR(256) 
+	DECLARE @fileName VARCHAR(256)  
+	DECLARE @fileDate VARCHAR(20)
+
+	SET @name = 'DatabaseName'
+	SET @path = 'D:\Backup\'
+	SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
+	SET @fileName = @path + @name + '_' + @fileDate + '.BAK'
+	
+	BACKUP DATABASE @name 
+	TO DISK = @fileName
+	WITH CHECKSUM
+
+	RESTORE VERIFYONLY
+	FROM DISK = @fileName
+
+	IF @@ERROR = 0
+	BEGIN
+		
+		-- Database backed up twice and both backups were verified
+		INSERT INTO admin.AdministrativeTasks
+		SELECT 'Backup File Created and Verified', GETDATE()
+	
+		PRINT 'Database backed up and verified ' + CAST(@count AS VARCHAR(1))
+
+	END
+	ELSE
+	BEGIN
+		
+		-- Either the backups failed, or the backups weren't verified
+		INSERT INTO admin.AdministrativeTasks
+		SELECT 'Either Database Not Backed Up Or Verified', GETDATE()
+
+		PRINT 'Either Database Not Backed Up Or Verified'
+	
+	END
+	
+END
+ELSE
+BEGIN
+
+	-- Database integrity compromised
+	PRINT 'Database integrity compromised'
+	INSERT INTO admin.AdministrativeTasks
+	SELECT 'ERROR! Database Integrity Compromised.', GETDATE()
+	
+END
+
+DROP TABLE ##DatabaseConsistency
 
 END
