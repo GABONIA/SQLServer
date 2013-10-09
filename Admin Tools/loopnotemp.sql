f0612004d1ae875b9ef9c4a23369b2d417177505
Tim <tmmtsmith@gmail.com>
2013-10-09 10:15:51 -0500

    Loop without temp table

---
+++ Admin Tools/loopnotemp.sql
@@ -0,0 +1,22 @@
+DECLARE @begin INT = 1, @max INT= 50, @sql NVARCHAR(MAX)
+
+WHILE @begin <= @max
+BEGIN
+
+	DECLARE @name VARCHAR(250)
+	;WITH n AS(
+		SELECT ROW_NUMBER() OVER (ORDER BY name) ID
+			, name
+		FROM sys.databases)
+	SELECT @name = name FROM n WHERE ID = @begin
+
+	SET @sql = 'SELECT ''' + @name + ''', name
+	FROM ' + @name + '.sys.tables
+	WHERE name LIKE ''%%'''
+
+	EXECUTE sp_executesql @sql
+
+	SET @begin = @begin + 1
+	SET @sql = ''
+
+END
