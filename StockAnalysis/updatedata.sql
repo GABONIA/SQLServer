7b40e62ca2dc4976b6ebcf2ab8b75e14f115a4ab
Tim <tmmtsmith@gmail.com>
2013-10-12 07:22:06 -0500

    Drop, C# update, Create

--- StockAnalysis/updatedata.sql
+++ StockAnalysis/updatedata.sql
@@ -16,4 +16,26 @@ END
 
 -- C# update
 
--- Rebuild indexes
+-- Step 3: Rebuild indexes
+
+CREATE PROCEDURE stp_UpdateData_RebuildIndexes
+@sym VARCHAR(200)
+AS
+BEGIN
+
+	DECLARE @sql NVARCHAR(MAX)
+	SET @sql = 'CREATE CLUSTERED INDEX [Date] ON [stock].[' + @sym + 'HistoricalData]
+	(
+		[Date] ASC
+	)WITH (PAD_INDEX = OFF, FILLFACTOR = 100, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+
+	PRINT ''Clustered index created''
+
+	CREATE NONCLUSTERED INDEX [' + @sym + 'ID] ON [stock].[' + @sym + 'HistoricalData]
+	(
+		[' + @sym + 'ID] ASC
+	)WITH (PAD_INDEX = OFF, FILLFACTOR = 100, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+
+	PRINT ''Non clustered index created'''
+
+END
