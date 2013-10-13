41e06adb74091466b84b19fc3c5513ddbc273e5f
Tim <tmmtsmith@gmail.com>
2013-10-13 06:50:32 -0500

    Update quadratic.sql

--- Teaching Examples (For SQL Server Classes)/quadratic.sql
+++ Teaching Examples (For SQL Server Classes)/quadratic.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE stp_QuadraticAlgorithm
-@a DECIMAL(22,4), @b DECIMAL(22,3), @c DECIMAL(22,4)
+@a DECIMAL(22,4), @b DECIMAL(22,3), @c DECIMAL(22,4), @vone VARCHAR(1), @vtwo VARCHAR(1)
 AS
 BEGIN
 
@@ -26,5 +26,27 @@ BEGIN
 	SELECT @stepeight StepSevenPlus, @stepnine StepSevenMinus
 
 	SELECT @xplus AS "X Plus", @xminus AS "X Minus"
+
+	DECLARE @sql NVARCHAR(MAX)
+
+	SET @sql = 'DECLARE @checkone DECIMAL(22,4), @checktwo DECIMAL(22,4)
+
+	SET @checkone = ((' + CAST(@a AS VARCHAR(30)) + '*(SQUARE(' + CAST(@xplus AS VARCHAR(30)) + '))) ' + @vone + ' (' + CAST(@b AS VARCHAR(30)) + '*(' + CAST(@xplus AS VARCHAR(30)) + ')) '  + @vtwo + ' ' + CAST(@c AS VARCHAR(30)) + ')
+	SET @checkone = ((' + CAST(@a AS VARCHAR(30)) + '*(SQUARE(' + CAST(@xminus AS VARCHAR(30)) + '))) ' + @vone + ' (' + CAST(@b AS VARCHAR(30))+ '*(' + CAST(@xminus AS VARCHAR(30)) + ')) '  + @vtwo + ' ' + CAST(@c AS VARCHAR(30)) + ')
+	
+	SELECT @checkone CheckXPlus, @checktwo CheckXMinus
+
+	IF @checkone = 0
+	BEGIN
+		PRINT ''X Plus is a solution''
+	END
+
+	IF @checktwo = 0
+	BEGIN
+		PRINT ''X Minus is a solution''
+	END
+	'
+
+	EXECUTE sp_executesql @sql
 
 END
