4ab2996609e2c094cd4fa4c9737f89204c3f3b0a
Tim <tmmtsmith@gmail.com>
2013-10-25 06:14:41 -0500

    Update transactionlog.sql

--- Admin Tools/transactionlog.sql
+++ Admin Tools/transactionlog.sql
@@ -4,10 +4,12 @@ Look Through the Transaction Log
 
 */
 
+
 SELECT SUSER_SNAME([Transaction SID]) As Changer
 	, [Transaction Name]
+	, [Begin Time]
 FROM fn_dblog(NULL, NULL)
-WHERE [Transaction Name] IN ('CREATE TABLE');
+WHERE [Transaction Name] IN ('CREATE TABLE','DROP TABLE');
 
 
 SELECT *
