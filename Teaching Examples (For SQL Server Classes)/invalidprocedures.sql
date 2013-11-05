4d0f5ff7029376cefc0d6d727136feba50fad011
Tim <tmmtsmith@gmail.com>
2013-11-05 12:43:26 -0600

    Find invalid procedures

--- Teaching Examples (For SQL Server Classes)/invalidprocedures.sql
+++ Teaching Examples (For SQL Server Classes)/invalidprocedures.sql
@@ -1,34 +1,40 @@
-/*
+USE SETRANS
+GO
 
-CREATE TABLE ##testprocs (
-	ProcID INT IDENTITY(1,1),
-	ProcName VARCHAR(250)
-)
+
+/*
 
 CREATE TABLE ##procname(
 	ProcName VARCHAR(250)
 )
 
-TRUNCATE TABLE ##testprocs
 TRUNCATE TABLE ##procname
 
 */
 
+DECLARE @testprocs TABLE (
+	ProcID INT IDENTITY(1,1),
+	ProcName VARCHAR(250)
+)
 
 
-INSERT INTO ##testprocs (ProcName)
+INSERT INTO @testprocs (ProcName)
 SELECT name ProcName
 FROM sys.objects
 WHERE type_desc = 'SQL_STORED_PROCEDURE'
+	AND name NOT IN (SELECT ProcName FROM ##procname)
+	-- Errored procedures:
+	AND name NOT IN ('')
 
 
 DECLARE @begin INT = 1, @max INT, @name VARCHAR(250), @sql NVARCHAR(MAX)
-SELECT @max = COUNT(ProcID) FROM ##testprocs
+SELECT @max = COUNT(ProcID) FROM @testprocs
+
 
 WHILE @begin <= @max
 BEGIN
 	
-	SELECT @name = ProcName FROM ##testprocs WHERE ProcID = @begin
+	SELECT @name = ProcName FROM @testprocs WHERE ProcID = @begin
 	PRINT 'Testing procedure ' + @name + ' for errors ...'
 	SET @sql = 'EXEC sys.sp_refreshsqlmodule ''' + @name + ''''
 	
@@ -37,9 +43,6 @@ BEGIN
 
 	INSERT INTO ##procname
 	SELECT @name
-
-	DELETE FROM ##testprocs
-	WHERE ProcName = @name
 	
 	SET @begin = @begin + 1
 
@@ -48,16 +51,9 @@ END
 
 /*
 
-DELETE FROM ##testprocs
-WHERE ProcName = 'updateopenpositions'
-
-SELECT *
-FROM ##testprocs
-
 SELECT *
 FROM ##procname
 
-DROP TABLE ##testprocs
 DROP TABLE ##procname
 
 */
