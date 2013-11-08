cbfae18558b4979f99f16a8a5094f9761612ea3f
Tim <tmmtsmith@gmail.com>
2013-11-08 07:55:32 -0600

    Disk issues

---
+++ Admin Tools/System/chkdsk.ps1
@@ -0,0 +1,7 @@
+## Syntax chkdsk [DriveLetter]: [Option]
+chkdsk c: /F
+## Other options
+## /F corrects errors
+## /R locates bad sectors
+## /I (NTFS only) check on indexes
+## For more options enter "chkdsk /?"
