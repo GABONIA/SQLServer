6993090afaf8133db7bac73c85f96978ab1c2e9d
Tim <tmmtsmith@gmail.com>
2013-11-12 15:54:27 -0600

    Slow App, Quick SSMS

---
+++ Admin Tools/TroubleShooting/Procs.sql
@@ -0,0 +1,37 @@
+/* (1) */
+
+DECLARE @inside VARCHAR(25)
+SET @inside = @outside
+
+-- USE @outside
+
+
+/* (2) */
+
+DBCC FREEPROCCACHE
+
+
+/* (3) */
+
+EXECUTE stp_OurProcedure WITH RECOMPILE
+
+
+/* (4) */
+
+CREATE NONCLUSTERED INDEX IX_MatchTheQuery ON OurTable(OurColumn)
+
+
+/* (5) */
+-- 2005, 2008R2
+
+EXECUTE AS USER = 'User\User';
+EXECUTE AS LOGIN = 'User';
+
+SELECT SUSER_NAME(), USER_NAME()
+
+EXECUTE stp_OurProcedure 'Parameter'
+
+REVERT
+
+/* (6) */
+EXEC sp_configure 'remote login timeout'
