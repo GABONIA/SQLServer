980ca4507a2d9f717c618a9f2debf2482d10f307
Tim <tmmtsmith@gmail.com>
2013-11-22 04:35:52 -0600

    Compare two tables

--- Admin Tools/TroubleShooting/tablecompare.sql
+++ Admin Tools/TroubleShooting/tablecompare.sql
@@ -17,3 +17,36 @@ FROM (
       SELECT ColumnOne, ColumnTwo, ColumnThree
       FROM dbo.Table
      ) AS R
+
+/* 
+
+-- Easy approach:
+
+DECLARE @t1 VARCHAR(250), @t2 VARCHAR(250), @cols VARCHAR(2000), @sql NVARCHAR(MAX)
+SET @t1 = 'TOne'
+SET @t2 = 'TTwo'
+SET @cols = 'I'
+
+SET @sql = '
+SELECT * 
+FROM (
+      SELECT ' + @cols + ' 
+      FROM ' + @t1 + '
+      EXCEPT
+      SELECT ' + @cols + ' 
+      FROM ' + @t2 + '
+     ) AS R
+UNION ALL
+SELECT * 
+FROM (
+      SELECT ' + @cols + ' 
+      FROM ' + @t2 + '
+      EXCEPT
+      SELECT ' + @cols + ' 
+      FROM ' + @t1 + '
+     ) AS R'
+
+EXEC sp_executesql @sql
+
+
+*/
