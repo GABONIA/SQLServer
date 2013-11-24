028cda125fb53f3c11c9a2a977e9ef5d91039b26
Tim <tmmtsmith@gmail.com>
2013-11-24 05:20:55 -0600

    Job result parse

---
+++ Admin Tools/jobresultparse.sql
@@ -0,0 +1,19 @@
+CREATE TABLE ##JobMessage(
+  JobMessage VARCHAR(MAX)
+)
+
+;WITH JobEm AS(
+  SELECT h.message JobMessage
+    , CONVERT(DATE,CONVERT(VARCHAR(10),h.run_date)) JobDate
+  FROM msdb.dbo.sysjobshistory h
+  -- Edit the value below this
+  WHERE j.name = 'JOBNAME'
+    AND h.step_name <> '(Job outcome)'
+)
+INSERT INTO ##JobMessage
+SELECT JobMessage
+FROM JobEm
+-- Edit
+WHERE JobMessage LIKE '%%'
+  AND JobDate BETWEEN DATEADD(DD,-1,GETDATE()) AND GETDATE()
+ORDER BY JobDate
