ffceb8d0dc027eb8a5cc5ec0078557b62f6c38b3
Tim <tmmtsmith@gmail.com>
2013-12-02 09:58:47 -0600

    Find object by text

---
+++ Admin Tools/Cleaning/findobject.sql
@@ -0,0 +1,65 @@
+CREATE PROCEDURE stp_FindTextInObjects
+@find VARCHAR(100)
+AS
+BEGIN
+
+	/* 
+	-- Note: this procedure will find a procedure in any database on a server (outside of master, msdb, model, tempdb)
+	-- Returns the Database and ObjectID
+	-- For Testing Purposes:
+	DECLARE @find VARCHAR(100)
+	SET @find = 'xp_cmdshell'
+	*/
+
+	DECLARE @loop TABLE(
+		DatabaseID INT IDENTITY(1,1),
+		DatabaseName VARCHAR(250)
+	)
+
+
+	CREATE TABLE ##Saved(
+		DatabaseName VARCHAR(250),
+		ObjectID BIGINT
+	)
+
+
+	INSERT INTO @loop (DatabaseName)
+	SELECT name
+	FROM master.sys.databases
+	WHERE database_id NOT IN (1,2,3,4)
+
+
+	DECLARE @begin INT = 1, @max INT, @d VARCHAR(250), @sql NVARCHAR(MAX)
+	SELECT @max = MAX(DatabaseID) FROM @loop
+
+	WHILE @begin <= @max
+	BEGIN
+
+		SELECT @d = DatabaseName FROM @loop WHERE DatabaseID = @begin
+
+		SET @sql = ';WITH CheckAll AS(
+			SELECT id ObjectID
+				, text ObjectText
+			FROM ' + @d + '.sys.syscomments
+			WHERE text LIKE ''%' + @find + '%''
+		)
+		INSERT INTO ##Saved (ObjectID,DatabaseName)
+		SELECT DISTINCT ObjectID
+			, ''' + @d + '''
+		FROM CheckAll'
+
+		EXECUTE sp_executesql @sql
+
+		SET @begin = @begin + 1
+		SET @d = ''
+
+	END
+
+
+	SELECT *
+	FROM ##Saved
+
+
+	DROP TABLE ##Saved
+
+END
