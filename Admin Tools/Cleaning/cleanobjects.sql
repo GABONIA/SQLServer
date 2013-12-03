54ddd2ab5ae6139fb0a5a4fa84695d16bcbeaf64
Tim <tmmtsmith@gmail.com>
2013-12-03 12:28:45 -0600

    Test Procedure, View, Function validity

--- Admin Tools/Cleaning/cleanobjects.sql
+++ Admin Tools/Cleaning/cleanobjects.sql
@@ -4,6 +4,8 @@
 
 */
 
+SET NOCOUNT ON
+
 DECLARE @loop TABLE(
 	ID INT IDENTITY(1,1),
 	ExecuteIt NVARCHAR(MAX)
@@ -11,12 +13,12 @@ DECLARE @loop TABLE(
 
 INSERT INTO @loop
 -- Procedures
-SELECT 'sys.sp_refreshsqlmodule ''' + name + ''' -- Procedure'
+SELECT 'sys.sp_refreshsqlmodule ''' 
+	+ QUOTENAME(SCHEMA_NAME(schema_id)) 
+	+ '.' + QUOTENAME(name) + ''' -- Proc'
 FROM sys.objects
 WHERE is_ms_shipped = 0
 	AND type_desc = 'SQL_STORED_PROCEDURE'
-	-- Filer out issues (one-by-one):
-	-- AND name <> ''
 
 INSERT INTO @loop
 -- Views
@@ -24,8 +26,6 @@ SELECT 'sys.sp_refreshsqlmodule ''' + name + ''' -- View'
 FROM sys.objects
 WHERE is_ms_shipped = 0
 	AND type_desc = 'VIEW'
-	-- Filer out issues (one-by-one):
-	-- AND name <> ''
 
 INSERT INTO @loop
 -- Functions
@@ -33,8 +33,6 @@ SELECT 'sys.sp_refreshsqlmodule ''' + name + ''' -- Function'
 FROM sys.objects
 WHERE is_ms_shipped = 0
 	AND type_desc = 'SQL_SCALAR_FUNCTION'
-	-- Filer out issues (one-by-one):
-	-- AND name <> ''
 
 DECLARE @begin INT = 1, @max INT, @sql NVARCHAR(MAX)
 SELECT @max = MAX(ID) FROM @loop
@@ -45,8 +43,18 @@ BEGIN
 	SELECT @sql = ExecuteIt FROM @loop WHERE ID = @begin
 	
 	PRINT 'Testing ' + @sql
-	EXECUTE sp_executesql @sql
+
+	BEGIN TRY
+		EXECUTE sp_executesql @sql
+	END TRY
+	BEGIN CATCH
+		PRINT '
+			' + REPLACE(REPLACE(REPLACE(SUBSTRING(@sql,(CHARINDEX('[',@sql,3)+1),LEN(@sql)),'''',''),']',''),'[','') + ' failed.
+			'
+	END CATCH
 
 	SET @begin = @begin + 1
 
 END
+
+SET NOCOUNT OFF
