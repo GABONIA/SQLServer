fe72a0ef759a22ff6780b35ee3bfdf242d609a9a
Tim <tmmtsmith@gmail.com>
2013-12-09 17:37:46 -0600

    Truncate tables by FK constraints

---
+++ Admin Tools/Cleaning/fktruncate.sql
@@ -0,0 +1,26 @@
+-- No FK constraints
+DECLARE @loop TABLE(
+	TableID INT IDENTITY(1,1),
+	TableName VARCHAR(250)
+)
+
+;WITH CTE AS(
+	SELECT OBJECT_NAME(parent_object_id) Name
+	FROM sys.foreign_key_columns
+)
+INSERT INTO @loop (TableName)
+SELECT o.name
+FROM sys.objects o
+	INNER JOIN sys.tables t ON o.object_id = t.object_id
+WHERE o.name NOT IN (SELECT Name FROM CTE)
+	AND o.is_ms_shipped = 0
+	AND t.is_ms_shipped = 0
+
+
+-- FK constraints
+INSERT INTO @loop (TableName)
+SELECT o.name
+	, OBJECT_NAME(k.referenced_object_id) Referenced
+	, OBJECT_NAME(k.parent_object_id) Parent
+FROM sys.objects o
+	INNER JOIN sys.foreign_key_columns k ON o.object_id = k.parent_object_id
