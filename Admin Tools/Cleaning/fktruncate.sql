ed0ad3ce1e2982c6644c779ef18e2f5b8cdafc39
Tim <tmmtsmith@gmail.com>
2013-12-10 11:09:14 -0600

    Step 1

--- Admin Tools/Cleaning/fktruncate.sql
+++ Admin Tools/Cleaning/fktruncate.sql
@@ -5,16 +5,18 @@ DECLARE @loop TABLE(
 )
 
 ;WITH CTE AS(
-	SELECT OBJECT_NAME(parent_object_id) Name
-	FROM sys.foreign_key_columns
+	SELECT OBJECT_NAME(k.referenced_object_id) Referenced
+        , OBJECT_NAME(k.parent_object_id) Parent
+	FROM sys.objects o
+        INNER JOIN sys.foreign_key_columns k ON o.object_id = k.parent_object_id
 )
 INSERT INTO @loop (TableName)
 SELECT o.name
 FROM sys.objects o
-	INNER JOIN sys.tables t ON o.object_id = t.object_id
-WHERE o.name NOT IN (SELECT Name FROM CTE)
-	AND o.is_ms_shipped = 0
-	AND t.is_ms_shipped = 0
+        INNER JOIN sys.tables t ON o.object_id = t.object_id
+WHERE o.name NOT IN (SELECT Referenced FROM CTE)
+        AND o.is_ms_shipped = 0
+        AND t.is_ms_shipped = 0
 
 
 -- FK constraints, order by dependency
