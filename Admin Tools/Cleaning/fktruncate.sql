cfbfcb302d54987c3a72987949659f23df831acf
Tim <tmmtsmith@gmail.com>
2013-12-10 11:15:22 -0600

    Update fktruncate.sql

--- Admin Tools/Cleaning/fktruncate.sql
+++ Admin Tools/Cleaning/fktruncate.sql
@@ -26,3 +26,55 @@ SELECT o.name
 	, OBJECT_NAME(k.parent_object_id) Parent
 FROM sys.objects o
 	INNER JOIN sys.foreign_key_columns k ON o.object_id = k.parent_object_id
+
+
+
+
+
+
+
+/* 
+-- Testing:
+
+CREATE TABLE AssociativeTable(
+	UniqueID INT IDENTITY(1,1) PRIMARY KEY,
+	OtherTableID INT,
+	YetAnotherTableID VARCHAR(1),
+	CONSTRAINT FFK_OtherTableID UNIQUE(OtherTableID),
+	CONSTRAINT FFK_YetAnotherTableID UNIQUE(YetAnotherTableID)
+)
+
+
+CREATE TABLE OtherTable(
+	OtherTableID INT PRIMARY KEY,
+	Name VARCHAR(50),
+	FOREIGN KEY (OtherTableID) REFERENCES AssociativeTable (OtherTableID)
+)
+
+CREATE TABLE YetAnotherTable(
+	YetAnotherTableID VARCHAR(1) PRIMARY KEY,
+	Name VARCHAR(50), 
+	FOREIGN KEY (YetAnotherTableID) REFERENCES AssociativeTable (YetAnotherTableID)
+)
+
+/* 
+
+DROP TABLE AssociativeTable
+DROP TABLE OtherTable
+DROP TABLE YetAnotherTable
+
+*/
+
+
+INSERT INTO AssociativeTable (OtherTableID, YetAnotherTableID)
+VALUES (1,'A')
+	, (2,'B')
+	, (3,'C')
+
+-- Works:
+INSERT INTO OtherTable
+VALUES (1,'Sarah')
+	, (2,'George')
+	, (3,'Bob')
+
+*/
