af9feb2e145a0dfa08845532174eaf4bdffa5493
Tim <tmmtsmith@gmail.com>
2013-12-25 07:24:33 -0600

    Update BulkInsert.sql

--- ETL/BulkInsert.sql
+++ ETL/BulkInsert.sql
@@ -1,15 +1,16 @@
 /*
 
 BULK INSERT Examples
-	-  Works with other file formats than .CSV and .TXT
+	-  Works with other file formats than .CSV and .TXT (files can have created formats like .cre and BULK INSERT will function).
 	-  Can complete skip the FIELDTERMINATOR if needed.
-	-  The ERRORFile will produce a log showing where the BULK INSERT is breaking (excellent for fixes)
+	-  The ERRORFile will produce a log showing where the BULK INSERT is breaking (excellent for fixes),
 
 */
 
 BULK INSERT TableName
 FROM 'C:\File.txt'
 WITH (
+	-- OPTIONS:
 	-- note, if only one column exists in file, this can be removed
 	FIELDTERMINATOR = ','
 	,ROWTERMINATOR = '0x0a'
@@ -18,5 +19,7 @@ WITH (
 	-- If the data end at a certain row
 	,LASTROW=1000000
 	-- Logs any rows of data causing an error (very useful for debugging)
-	,ERRORFILE='C:\logfile.log')
+	,ERRORFILE='C:\logfile.log'
+	-- Specifies how many errors in rows can occur before it will break (almost always set this to 0)
+	,MAXERRORS = 0)
 GO
