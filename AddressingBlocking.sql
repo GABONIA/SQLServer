47bd70e1059f60f37e57f298fbf504a3d98a1c98
Tim <tmmtsmith@gmail.com>
2013-12-31 07:13:47 -0600

    Address blocking

--- AddressingBlocking.sql
+++ AddressingBlocking.sql
@@ -1,5 +1,8 @@
-IF (SELECT COUNT(w.blocking_session_id) "Blocks"
-	FROM sys.dm_tran_locks t
-		INNER JOIN sys.dm_os_waiting_tasks w ON t.lock_owner_address = w.resource_address
-	WHERE w.blocking_session_id IS NOT NULL) > 25
---[SEND ALERT/KILL ALL PROCESSES]
+DECLARE @cnt INT
+SELECT @cnt = COUNT(w.blocking_session_id)  FROM sys.dm_tran_locks t  INNER JOIN sys.dm_os_waiting_tasks w ON t.lock_owner_address = w.resource_address
+
+
+IF @cnt > 25
+BEGIN
+	-- Blocking Process
+END
