1bb8f86cd37c0731d44f3e64c111c931586421e4
Tim <tmmtsmith@gmail.com>
2014-01-01 07:26:12 -0600

    Get fill factor information

--- Admin Tools/fillfactor.sql
+++ Admin Tools/fillfactor.sql
@@ -1,8 +1,9 @@
 ;WITH CheckFillFactor AS(
 	SELECT OBJECT_NAME(OBJECT_ID) [Name]
 		, type_desc [Type]
-		, fill_factor [Fill Factor]
+		, fill_factor [FF]
 	FROM sys.indexes
 )
 SELECT *
 FROM CheckFillFactor
+--WHERE FF = 100
