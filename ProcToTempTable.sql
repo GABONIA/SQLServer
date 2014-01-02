147237fced7ae11cbe913b2d0d85a77ee2469b0d
Tim <tmmtsmith@gmail.com>
2014-01-02 07:24:15 -0600

    Procedure result to temp table

--- ProcToTempTable.sql
+++ ProcToTempTable.sql
@@ -4,5 +4,7 @@ Select query from a stored procedure into a temporary table
 
 */
 
-SELECT * INTO #zTempTablez FROM OPENROWSET('SQLNCLI', 'Server=SERVER\INSTANCE;Catalog=DATABASENAME;Trusted_Connection=yes;',
-     'EXECUTE dbo.stp_StoredProcedureName')
+SELECT * INTO #zTempTablez 
+FROM OPENROWSET('SQLNCLI'
+     , 'Server=SERVER\INSTANCE;Catalog=DATABASENAME;Trusted_Connection=yes;'
+     ,'EXECUTE dbo.stp_StoredProcedureName')
