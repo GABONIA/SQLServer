d4e3396bbf3a10dda3e2b9a4996ffe7b06defd4d
Tim <tmmtsmith@gmail.com>
2014-01-04 07:06:05 -0600

    Pictures

--- picture.sql
+++ picture.sql
@@ -2,6 +2,15 @@ CREATE TABLE Pictures(
   Picture VARBINARY(MAX)
 )
 
+-- C#
+
 INSERT INTO Pictures (Picture)
-SELECT * FROM 
-OPENROWSET(BULK N'C:\Pictures\20140101.jpg', SINGLE_BLOB) 
+SELECT * 
+FROM OPENROWSET(BULK N'C:\Pictures\20140101.jpg', SINGLE_BLOB) 
+
+/*
+-- Non-Proc
+
+"INSERT INTO Pictures (Picture) SELECT * FROM OPENROWSET(BULK N'" + @f + "', SINGLE_BLOB)"
+
+*/
