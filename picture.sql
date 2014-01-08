0ef3d2e531c84bad977604fcabf8212dacfe11fe
Tim <tmmtsmith@gmail.com>
2014-01-08 06:51:53 -0600

    Update picture.sql

--- picture.sql
+++ picture.sql
@@ -11,6 +11,8 @@ FROM OPENROWSET(BULK N'C:\Pictures\20140101.jpg', SINGLE_BLOB)
 /*
 -- Non-Proc
 
+//foreach (file in files)
+
 "INSERT INTO Pictures (Picture) SELECT * FROM OPENROWSET(BULK N'" + @f + "', SINGLE_BLOB)"
 
 */
