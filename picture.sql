7542278465f7b7ef4672709a02ffce3f349bab55
Tim <tmmtsmith@gmail.com>
2014-01-11 07:49:51 -0600

    Update picture.sql

--- picture.sql
+++ picture.sql
@@ -15,4 +15,20 @@ FROM OPENROWSET(BULK N'C:\Pictures\20140101.jpg', SINGLE_BLOB) ps -- error witho
 
 "INSERT INTO Pictures (Picture) SELECT * FROM OPENROWSET(BULK N'" + @f + "', SINGLE_BLOB)"
 
+
+CREATE PROCEDURE stp_AddImage
+@f VARCHAR(1000)
+AS
+BEGIN
+
+  DECLARE @s NVARCHAR(MAX)
+  SET @s = 'INSERT INTO Pictures (Picture)
+    SELECT *
+    FROM OPENROWSET(BULK N''' + @f + ''', SINGLE_BLOG)'
+    
+  EXEC sp_executesql @s
+
+END
+
+
 */
