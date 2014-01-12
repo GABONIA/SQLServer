3343adb339433f14620c1d84b1ef8f03d40c8d68
Tim <tmmtsmith@gmail.com>
2014-01-12 07:21:29 -0600

    C# Loop

--- picture.sql
+++ picture.sql
@@ -24,7 +24,7 @@ BEGIN
   DECLARE @s NVARCHAR(MAX)
   SET @s = 'INSERT INTO Pictures (Picture)
     SELECT *
-    FROM OPENROWSET(BULK N''' + @f + ''', SINGLE_BLOG)'
+    FROM OPENROWSET(BULK N''' + @f + ''', SINGLE_BLOG) mf'
     
   EXEC sp_executesql @s
 
@@ -32,3 +32,53 @@ END
 
 
 */
+
+-- C# Loop
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace PictureInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"F:\Images\";
+            string[] fs = Directory.GetFiles(dir);
+
+            foreach (string f in fs)
+            {
+                if (f.Trim().EndsWith(".png") | f.Trim().EndsWith(".jpg"))
+                {
+                    using (var scon = Connection.Connect())
+                    {
+                        SqlCommand addpic = new SqlCommand("EXECUTE stp_AddImage @p", scon);
+                        addpic.Parameters.AddWithValue("@p", f);
+                        addpic.ExecuteNonQuery();
+                        scon.Close();
+                    }
+                    Console.WriteLine("Image " + f + " added." + "\n");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public static class Connection
+    {
+        public static SqlConnection Connect()
+        {
+            SqlConnection scon = new SqlConnection();
+            scon.ConnectionString = "integrated security=SSPI;data source=SERVER;persist security info=False;initial catalog=DATABASE";
+            scon.Open();
+            return scon;
+        }
+    }
+}
+
