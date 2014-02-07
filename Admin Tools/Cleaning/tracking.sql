832e617f426171b6d83323b0bd193620b58b05c1
Tim <tmmtsmith@gmail.com>
2014-02-07 09:57:28 -0600

    Track jobs, linked servers, procedures by ref

--- Admin Tools/Cleaning/tracking.sql
+++ Admin Tools/Cleaning/tracking.sql
@@ -4,6 +4,8 @@ DECLARE @find VARCHAR(100)
 -- Keyword
 SET @find = 'DEV001'
 
+
+
 CREATE TABLE ##zzLinked(
 	SRV_NAME VARCHAR(100),
 	SRV_PROVIDERNAME VARCHAR(250),
@@ -23,8 +25,55 @@ WHERE SRV_DATASOURCE LIKE '%' + @find + '%'
 
 DROP TABLE ##zzLinked
 
+
+
 SELECT DISTINCT j.name
 FROM msdb.dbo.sysjobsteps s
 	INNER JOIN msdb.dbo.sysjobs j ON s.job_id = j.job_id
 WHERE s.step_name LIKE '%' + @find + '%'
 	OR s.command LIKE '%' + @find + '%'
+
+
+
+DECLARE @loop TABLE(
+	ID INT IDENTITY(1,1),
+	Name NVARCHAR(200)
+)
+
+CREATE TABLE ##r(
+	ID INT IDENTITY(1,1),
+	Name NVARCHAR(200)
+)
+
+INSERT INTO @loop
+SELECT name
+FROM sys.databases
+WHERE database_id NOT IN (1,2,3,4)
+
+DECLARE @b INT = 1, @m INT, @d NVARCHAR(100), @s NVARCHAR(MAX)
+SELECT @m = MAX(ID) FROM @loop
+
+WHILE @b <= @m
+BEGIN
+
+	SELECT @d = Name FROM @loop WHERE ID = @b
+
+	SET @s = 'DECLARE @f NVARCHAR(100)
+	SET @f = @find
+
+	INSERT INTO ##r (Name)
+	SELECT name
+	FROM ' + @d + '.sys.procedures p
+		INNER JOIN sys.syscomments c ON p.object_id = c.id
+	WHERE c.text LIKE ''%'' + @f + ''%'''
+
+	EXEC sp_executesql @s, N'@find NVARCHAR(100)',@find
+
+	SET @b = @b + 1
+
+END
+
+SELECT *
+FROM ##r
+
+DROP TABLE ##r
