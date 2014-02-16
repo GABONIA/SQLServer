238b4c81336e4f423247ce787f87aaa649cbe16f
Tim <tmmtsmith@gmail.com>
2014-02-16 10:35:12 -0600

    Find unused jobs in one (or more) servers

---
+++ Admin Tools/Cleaning/unusedjobs.ps1
@@ -0,0 +1,36 @@
+Function FindUnusedJobs ($server)
+{
+    $s = New-Object "Microsoft.SqlServer.Management.Smo.Server" $server
+    $d = Get-Date -uFormat '%Y%m%d'
+
+    foreach ($j in $s.JobServer.Jobs)
+    {
+        if ($y.Length -lt 4)
+        {
+            Write-Host $j.Name
+        }
+    }
+}
+
+FindOldOrUnusedJobs -server "SERVER\INSTANCE"
+
+<#
+## Loop through multiple servers approach:
+
+$ss = "SERVER\INSTANCE","SERVER2\INSTANCE2"
+
+foreach ($s in $ss)
+{
+    $srv = New-Object "Microsoft.SqlServer.Management.Smo.Server" $s
+    foreach ($j in $srv.JobServer.Jobs)
+    {
+        $y = $j.LastRunDate.Year.ToString()
+        if ($y.Length -lt 4)
+        {
+            Write-Host Server $s Job $j.Name
+        }
+    }
+    $srv = ""
+}
+
+#>
