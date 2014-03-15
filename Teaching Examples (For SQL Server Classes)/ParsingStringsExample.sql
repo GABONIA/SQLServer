a040b5ea1680a104211da971c314ec1efd858265
Tim <tmmtsmith@gmail.com>
2014-03-14 21:20:26 -0500

    Parsing strings between commas

--- Teaching Examples (For SQL Server Classes)/ParsingStringsExample.sql
+++ Teaching Examples (For SQL Server Classes)/ParsingStringsExample.sql
@@ -6,15 +6,17 @@ Example: Parsing values out of a string
 
 DECLARE @comma VARCHAR(75)
 SET @comma = 'hello,my,name,is,comma,and,i,am,unable,to,say,separate,'
-DECLARE @begin INT = 0
-DECLARE @count INT = 0
+DECLARE @begin INT = 0, @count INT = 0, @max INT
+SELECT @max = LEN(@comma) - LEN(REPLACE(@comma,',',''))
 
-WHILE @count < 12
+WHILE @count < @max
 BEGIN
+
 	-- Parses every string except "separate" unless comma follows it
 	SELECT REPLACE(SUBSTRING(@comma,@begin,CHARINDEX(',',@comma,@begin+1)-@begin),',','')
 	SELECT CHARINDEX(',',@comma,@begin+1)
 	SET @begin = CHARINDEX(',',@comma,@begin+1)
-	
+
 	SET @count = @count + 1
+
 END
