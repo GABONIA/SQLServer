a4cbecfe28295c399e099e23b97ee76de31c584d
Tim <tmmtsmith@gmail.com>
2014-03-24 10:50:05 -0500

    Automated two column pivot table

--- Admin Tools/OutputColumns.sql
+++ Admin Tools/OutputColumns.sql
@@ -24,3 +24,30 @@ BEGIN
 	EXECUTE sp_executesql @s
 
 END
+
+-- For an automatic two column Pivot table
+CREATE PROCEDURE stp_TwoColumnPivot
+@PivotColumn VARCHAR(250),
+@CalcColumn VARCHAR(250),
+@SourceTable VARCHAR(250),
+@Function VARCHAR(10)
+AS
+BEGIN
+
+	DECLARE @c NVARCHAR(4000), @sql NVARCHAR(MAX)
+
+	DECLARE @s TABLE(
+		S NVARCHAR(4000)
+	)
+	
+	INSERT INTO @s
+	EXECUTE stp_OutputColumns @PivotColumn, @SourceTable
+	
+	SELECT @c = S FROM @s
+
+	SET @sql = N'SELECT ' + @c + ' FROM (SELECT t.' + @PivotColumn + ', t.' + @CalcColumn + ' FROM ' + @SourceTable + ' t) p
+				PIVOT (' + @Function + '(' + @CalcColumn + ') FOR ' + @PivotColumn + ' IN (' + @c + ')) AS pv;'
+				
+	EXEC sp_executesql @sql
+	
+END
