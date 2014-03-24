c4b7264e21dd01fda09b34a1b3ffa44444c567f5
Tim <tmmtsmith@gmail.com>
2014-03-24 14:46:31 -0500

    Two and three column automatics

--- Admin Tools/OutputColumns.sql
+++ Admin Tools/OutputColumns.sql
@@ -25,7 +25,7 @@ BEGIN
 
 END
 
--- For an automatic two column Pivot table
+-- For an automatic two-to-many column output pivot table
 CREATE PROCEDURE stp_TwoColumnPivot
 @PivotColumn VARCHAR(250),
 @CalcColumn VARCHAR(250),
@@ -46,6 +46,34 @@ BEGIN
 	SELECT @c = S FROM @s
 
 	SET @sql = N'SELECT ' + @c + ' FROM (SELECT t.' + @PivotColumn + ', t.' + @CalcColumn + ' FROM ' + @SourceTable + ' t) p
+				PIVOT (' + @Function + '(' + @CalcColumn + ') FOR ' + @PivotColumn + ' IN (' + @c + ')) AS pv;'
+				
+	EXEC sp_executesql @sql
+	
+END
+
+-- For an automatic three-to-many column output pivot table
+CREATE PROCEDURE stp_ThreeColumnPivot
+@PivotColumn VARCHAR(250),
+@CalcColumn VARCHAR(250),
+@NonPivotColumn VARCHAR(250),
+@SourceTable VARCHAR(250),
+@Function VARCHAR(10)
+AS
+BEGIN
+
+	DECLARE @c NVARCHAR(4000), @sql NVARCHAR(MAX)
+
+	DECLARE @s TABLE(
+		S NVARCHAR(4000)
+	)
+	
+	INSERT INTO @s
+	EXECUTE stp_OutputColumns @PivotColumn, @SourceTable
+	
+	SELECT @c = S FROM @s
+
+	SET @sql = N'SELECT ' + @NonPivotColumn + ', ' + @c + ' FROM (SELECT t.' + @NonPivotColumn + ', t.' + @PivotColumn + ', t.' + @CalcColumn + ' FROM ' + @SourceTable + ' t) p
 				PIVOT (' + @Function + '(' + @CalcColumn + ') FOR ' + @PivotColumn + ' IN (' + @c + ')) AS pv;'
 				
 	EXEC sp_executesql @sql
