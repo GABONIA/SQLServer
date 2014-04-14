594be7850ad56af09b1e030765f8b1883e920733
Tim <tmmtsmith@gmail.com>
2014-04-14 04:42:23 -0500

    No need for if exists

--- ETL/removeoutliers.sql
+++ ETL/removeoutliers.sql
@@ -91,26 +91,18 @@ BEGIN
 	SET @to = QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers')
 
 	DECLARE @s NVARCHAR(MAX)
-	SET @s = N'IF OBJECT_ID(@to) IS NULL
-	BEGIN
-		;WITH OutOutlier AS(
-			SELECT ' + @id + ' NewestID
-				, ' + QUOTENAME(@v) + ' OutValue
-				, (' + @avg + ' + (' + @stdev + ' *' + CAST(@dev AS NVARCHAR(3)) + ')) OAbove
-				, (' + @avg + ' + (' + @stdev + ' *-' + CAST(@dev AS NVARCHAR(3)) + ')) OBelow
-			FROM ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t) + '
-		)
-		SELECT t2.*
-		FROM OutOutlier t
-			INNER JOIN ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t) + ' t2 ON t.NewestID = t2.' + @id + '
-		WHERE t.OutValue BETWEEN OBelow AND OAbove
-		
-	END
-	ELSE
-	BEGIN
-		PRINT @to + '' exists.''
-	END'
+	SET @s = N';WITH OutOutlier AS(
+		SELECT ' + @id + ' NewestID
+			, ' + QUOTENAME(@v) + ' OutValue
+			, (' + @avg + ' + (' + @stdev + ' *' + CAST(@dev AS NVARCHAR(3)) + ')) OAbove
+			, (' + @avg + ' + (' + @stdev + ' *-' + CAST(@dev AS NVARCHAR(3)) + ')) OBelow
+		FROM ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t) + '
+	)
+	SELECT t2.*
+	FROM OutOutlier t
+		INNER JOIN ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t) + ' t2 ON t.NewestID = t2.' + @id + '
+	WHERE t.OutValue BETWEEN OBelow AND OAbove'
 
-	EXEC sp_executesql @s,N'@to NVARCHAR(100)',@to
+	EXEC sp_executesql @s
 
 END
