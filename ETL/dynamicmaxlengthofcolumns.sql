1dffccfbce650bef8ad71cde6c1646be1b83dc36
Tim <tmmtsmith@gmail.com>
2014-04-17 17:15:02 -0500

    Dynamically obtains max length of columns

---
+++ ETL/dynamicmaxlengthofcolumns.sql
@@ -0,0 +1,6 @@
+DECLARE @aCount NVARCHAR(MAX) = ''
+
+
+SELECT @aCount += 'MAX(LEN(' + COLUMN_NAME + ')), ' FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'OurTable'
+SELECT @aCount = 'SELECT ' + LEFT(@aCount, LEN(@aCount)-1) + ' FROM OurTable'
+EXECUTE(@aCount)
