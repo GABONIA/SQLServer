4fe6ce7b7440657cd75d10268831e5a9d93aa808
Tim <tmmtsmith@gmail.com>
2014-04-17 17:43:38 -0500

    Dynamically set column length

---
+++ ETL/setcolumnlength.sql
@@ -0,0 +1,32 @@
+DECLARE @t NVARCHAR(100) = 'stage_MLS_CoreLogic'
+
+DECLARE @loop TABLE (LoopID INT IDENTITY(1,1), ColumnName NVARCHAR(250))
+
+INSERT INTO @loop (ColumnName)
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = @t
+
+DECLARE @b INT = 1, @m INT, @c NVARCHAR(250), @s NVARCHAR(MAX), @s1 NVARCHAR(MAX), @len NVARCHAR(250)
+SELECT @m = MAX(LoopID) FROM @loop
+
+WHILE @b <= @m
+BEGIN
+
+	SELECT @c = ColumnName FROM @loop WHERE LoopID = @b
+	
+	SET @s1 = 'SELECT LEN(' + QUOTENAME(@c) + ') FROM ' + QUOTENAME(@t)
+
+	DECLARE @lentab TABLE (LenVal INT)
+	INSERT INTO @lentab EXECUTE(@s1)
+	SELECT @len = LenVal FROM @lentab
+	SET @len = @len + 2
+
+	SET @s = 'ALTER TABLE ' + QUOTENAME(@t) + ' ALTER COLUMN ' + QUOTENAME(@c) + ' VARCHAR(' + @len + ')'
+	
+	--PRINT @s
+	EXECUTE sp_executesql @s
+
+	SET @b = @b + 1
+
+END
