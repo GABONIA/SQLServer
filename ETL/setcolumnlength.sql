e03031571055d3cc44a6c45eb88056d76b73edec
Tim <tmmtsmith@gmail.com>
2014-04-21 15:34:16 -0500

    Update setcolumnlength.sql

--- ETL/setcolumnlength.sql
+++ ETL/setcolumnlength.sql
@@ -30,3 +30,54 @@ BEGIN
 	SET @b = @b + 1
 
 END
+
+/*
+
+CREATE PROCEDURE stp_AutoSetColumnLength
+@t NVARCHAR(200)
+AS
+BEGIN
+
+	DECLARE @loop TABLE (LoopID INT IDENTITY(1,1), ColumnName NVARCHAR(250))
+
+	INSERT INTO @loop (ColumnName)
+	SELECT COLUMN_NAME
+	FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE TABLE_NAME = @t
+
+	DECLARE @b INT = 1, @m INT, @c NVARCHAR(250), @s NVARCHAR(MAX), @s1 NVARCHAR(MAX), @len NVARCHAR(250)
+	SELECT @m = MAX(LoopID) FROM @loop
+
+	WHILE @b <= @m
+	BEGIN
+
+		SELECT @c = ColumnName FROM @loop WHERE LoopID = @b
+
+		SET @s1 = 'SELECT LEN(' + QUOTENAME(@c) + ') FROM ' + QUOTENAME(@t)
+
+		DECLARE @lentab TABLE (LenVal INT)
+		INSERT INTO @lentab EXECUTE(@s1)
+		SELECT @len = LenVal FROM @lentab
+
+		IF @len < 10
+		BEGIN
+			SET @len = 15
+		END
+		ELSE
+		BEGIN
+			SET @len = @len + 25
+		END
+
+		SET @s = 'ALTER TABLE ' + QUOTENAME(@t) + ' ALTER COLUMN ' + QUOTENAME(@c) + ' VARCHAR(' + @len + ')'
+
+		--PRINT @s
+		EXECUTE sp_executesql @s
+
+		SET @b = @b + 1
+
+	END
+
+END
+
+
+*/
