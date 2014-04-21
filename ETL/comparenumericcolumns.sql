ba44fc8b8c8f16bd24948b098e2a7c6702fc7326
Tim <tmmtsmith@gmail.com>
2014-04-21 11:01:38 -0500

    Compare numeric columns (cast as decimals)

--- ETL/comparenumericcolumns.sql
+++ ETL/comparenumericcolumns.sql
@@ -11,7 +11,7 @@
 
 */
 
-
+-- Assumes ID value
 CREATE PROCEDURE stp_CompareNumericColumns
 @c1 NVARCHAR(250),
 @c2 NVARCHAR(250),
@@ -21,6 +21,34 @@ BEGIN
 
 	DECLARE @s NVARCHAR(MAX), @id NVARCHAR(25)
 	SELECT @id = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @t AND COLUMN_NAME LIKE '%ID%'
+	
+	SET @s = 'SELECT ' + QUOTENAME(@id) + '
+		, ' + QUOTENAME(@c1) + '
+		, ' + QUOTENAME(@c2) + '
+	FROM ' + QUOTENAME(@t) + '
+	WHERE CAST(' + QUOTENAME(@c1) + ' AS DECIMAL(13,4)) NOT BETWEEN (CAST(' + QUOTENAME(@c2) + ' AS DECIMAL(13,4)) - 0.5) AND (CAST(' + QUOTENAME(@c2) + ' AS DECIMAL(13,4)) + 0.5)
+		AND ' + QUOTENAME(@c1) + ' IS NOT NULL
+		AND ' + QUOTENAME(@c2) + ' IS NOT NULL
+		AND ' + QUOTENAME(@c1) + ' <> ''''
+		AND ' + QUOTENAME(@c2) + ' <> ''''
+	'
+
+	--PRINT @s
+	EXECUTE sp_executesql @s
+
+END
+
+
+-- Pass in ID value
+CREATE PROCEDURE stp_CompareNumericColumns
+@c1 NVARCHAR(250),
+@c2 NVARCHAR(250),
+@t NVARCHAR(250),
+@id NVARCHAR(25)
+AS
+BEGIN
+
+	DECLARE @s NVARCHAR(MAX)
 	
 	SET @s = 'SELECT ' + QUOTENAME(@id) + '
 		, ' + QUOTENAME(@c1) + '
