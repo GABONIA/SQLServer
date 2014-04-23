88ffdf5e59f421d8e10ffde21d07a6d4a912ae14
Tim <tmmtsmith@gmail.com>
2014-04-23 13:35:25 -0500

    Added reporting

--- ETL/setcolumnlength.sql
+++ ETL/setcolumnlength.sql
@@ -79,5 +79,41 @@ BEGIN
 
 END
 
+*/
+
+/* 
+-- Reporting
+
+SET NOCOUNT ON
+DECLARE @t NVARCHAR(100) = ''
+
+DECLARE @loop TABLE (LoopID INT IDENTITY(1,1), ColumnName NVARCHAR(250))
+
+INSERT INTO @loop (ColumnName)
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = @t
+
+DECLARE @b INT = 1, @m INT, @c NVARCHAR(250), @s NVARCHAR(MAX), @s1 NVARCHAR(MAX), @len NVARCHAR(250), @mess NVARCHAR(500)
+SELECT @m = MAX(LoopID) FROM @loop
+
+WHILE @b <= @m
+BEGIN
+	SELECT @c = ColumnName FROM @loop WHERE LoopID = @b
+
+	SET @s1 = 'SELECT MAX(LEN(ISNULL(' + QUOTENAME(@c) + ',0))) FROM ' + QUOTENAME(@t)
+
+	DECLARE @lentab TABLE (LenVal INT)
+	INSERT INTO @lentab EXECUTE(@s1)
+	SELECT @len = LenVal FROM @lentab
+	SET @mess = @c + ' length: ' + @len
+	RAISERROR(@mess,0,1) WITH NOWAIT
+
+	EXECUTE sp_executesql @s1
+
+	SET @b = @b + 1
+	SET @mess = ''
+END
+
 
 */
