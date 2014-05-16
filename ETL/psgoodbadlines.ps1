9887427748b554acff40dc163a82243bd37056d1
Tim <tmmtsmith@gmail.com>
2014-05-16 04:42:11 -0500

    Create psgoodbadlines.ps1

---
+++ ETL/psgoodbadlines.ps1
@@ -0,0 +1,16 @@
+## See: https://github.com/tmmtsmith/SQLServer/blob/master/ETL/CSharpWriteBadAndGoodLinesByDelimiter.cs
+
+Function GetLastLineNumber ($file)
+{
+    $readfile = New-Object System.IO.StreamReader($file)
+    $cnt = 0
+    while ($line = $readfile.ReadLine())
+    {
+        $cnt++
+    }
+    $readfile.Close()
+    $readfile.Dispose()
+    return $cnt
+}
+
+GetLastLineNumber -file "C:\files\OurBigFile.txt"
