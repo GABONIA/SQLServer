ec4f3cc77aff873acbd010f534c75db1f92138ee
Tim <tmmtsmith@gmail.com>
2014-06-02 12:10:42 -0500

    Update eloop.ps1

--- ETL/eloop.ps1
+++ ETL/eloop.ps1
@@ -1,8 +1,14 @@
-## Simple example of Excel cell loop with 5 rows and columns of data (expand if larger)
+## Simple example of Excel cell loo
 
-for ($c = 1; $c -lt 5; $c++)
+$maxrow = $first.UsedRange.Cells | Select-Object -Last 1 | Select-Object -Property Row
+$maxrow = $maxrow.Row
+$maxcol = $first.UsedRange.Cells | Select-Object -Last 1 | Select-Object -Property Column
+$maxcol = $maxcol.Column
+
+
+for ($c = 1; $c -le $maxrow = $maxrow.Row; $c++)
 {
-    for ($r = 1; $r -lt 5; $r++)
+    for ($r = 1; $r -le $maxrow; $r++)
     {
         if ($ws.Cells.Item($r,$c).Value() -like "*,*")  ## Or other character, like " ' |
         {
