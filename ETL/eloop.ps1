e029a669285e2a073e7449fcfca4a8fae9580ea7
Tim <tmmtsmith@gmail.com>
2014-06-02 11:26:35 -0500

    Update eloop.ps1

--- ETL/eloop.ps1
+++ ETL/eloop.ps1
@@ -4,7 +4,7 @@ for ($c = 1; $c -lt 5; $c++)
 {
     for ($r = 1; $r -lt 5; $r++)
     {
-        if ($first.Cells.Item($r,$c).Value() -like "*,*")  ## Or other character, like " ' |
+        if ($ws.Cells.Item($r,$c).Value() -like "*,*")  ## Or other character, like " ' |
         {
             ## Replace or change
         }
