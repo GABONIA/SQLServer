91e13edd3a39029f2ecfeecdde6c63202124406e
Tim <tmmtsmith@gmail.com>
2014-06-07 04:54:43 -0500

    Update psgoodbadlines.ps1

--- ETL/psgoodbadlines.ps1
+++ ETL/psgoodbadlines.ps1
@@ -31,6 +31,45 @@ Function GetLineByNumber ($file, $lineno)
 
 GetLineByNumber -file "C:\files\new.txt" -lineno 2
 
+Function SeparateGoodBadLines ($file, $ch, $validCount)
+{
+    $ext = $file.Substring($file.LastIndexOf("."))
+    $loc = $file.Substring(0,($file.LastIndexOf("\")+1))
+    $name = $file.Substring($file.LastIndexOf("\")+1).Replace($ext,"")
+
+    $valid = $loc + $name + "_valid" + $ext
+    $invalid = $loc + $name + "_invalid" + $ext
+
+    New-Item $valid -ItemType file
+    New-Item $invalid -ItemType file
+
+    $read = New-Object System.IO.StreamReader($file)
+    $wValid = New-Object System.IO.StreamWriter($valid)
+    $wInvalid = New-Object System.IO.StreamWriter($invalid)
+
+    while (($line = $read.ReadLine()) -ne $null)
+    {
+        $total = $line.Split($ch).Length - 1;
+        if ($total -eq $validCount)
+        {
+            $wValid.WriteLine($line)
+            $wValid.Flush()
+        }
+        else
+        {
+            $wInvalid.WriteLine($line)
+            $wInvalid.Flush()
+        }
+    }
+
+    $read.Close()
+    $read.Dispose()
+    $wValid.Close()
+    $wValid.Dispose()
+    $wInvalid.Close()
+    $wInvalid.Dispose()
+}
+
 <#
 
 $x = Get-Content "C:\files\OurBigFile.txt" | Measure-Object -Line | Select-Object -Property Lines
