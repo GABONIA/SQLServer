4293087976e33b225a6116c67b7d6a1b0303dc75
Tim <tmmtsmith@gmail.com>
2014-06-10 16:34:13 -0500

    Create verrors.sql

---
+++ Admin Tools/TroubleShooting/verrors.sql
@@ -0,0 +1,23 @@
+DECLARE @loop TABLE(
+	LoopID INT IDENTITY(1,1),
+	TableName VARCHAR(250)
+)
+
+INSERT INTO @loop (TableName)
+SELECT name
+FROM sys.views
+
+DECLARE @b INT = 1, @m INT, @t VARCHAR(250)
+SELECT @m = MAX(LoopID) FROM @loop
+
+WHILE @b <= @m
+BEGIN
+
+	SELECT @t = TableName FROM @loop WHERE LoopID = @b
+
+	PRINT @t
+	EXECUTE('SELECT TOP 1 * FROM ' + @t + ' WHERE 1=1')
+
+	SET @b = @b + 1
+
+END
