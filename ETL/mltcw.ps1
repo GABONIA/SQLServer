e1a9984255d28c484afc7752e754cfb4048fbc04
Tim <tmmtsmith@gmail.com>
2014-06-25 11:07:49 -0500

    Update mltcw.ps1

--- ETL/mltcw.ps1
+++ ETL/mltcw.ps1
@@ -1,25 +1,8 @@
-Function TSQL_MultipleCaseWhens ($min, $max, $value, $equation, $field)
+-- See tranf.
+Function MultipleStrings ($string)
 {
     $nl = [Environment]::NewLine
-    $times = $max / $value
-    $first = "CASE WHEN $equation < $min THEN 0" + $nl
-    $end = "WHEN $equation > $max THEN " + ($times + 1).ToString() + " END AS $field" + $nl
-    $between = ""
-
-    for ($i = 1; $i -le $times; $i++)
-    {
-        if ($i -eq 1)
-        { 
-            $between += "WHEN $equation > $min AND $equation <= $value THEN $i" + $nl
-        }
-        else
-        {
-            $between += "WHEN $equation > $prev_value AND $equation <= $value THEN $i" + $nl
-        }
-        $prev_value = $value
-    }
-    $all = $first + $between + $end
-    $all
+    $string*3
 }
 
-MultipleStrings -min 0 -max 100000 -value 10000 -equation "(([ColumnOne]*[ColumnTwo])/[ColumnThree])" -field "OurFinalColumn"
+MultipleStrings -string
