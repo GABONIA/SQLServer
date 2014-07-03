c681f829e0d773dd72664813363f00a3c96a9a1c
Tim <tmmtsmith@gmail.com>
2014-07-03 07:11:07 -0500

    Create 06232014.sql

---
+++ Teaching Examples (For SQL Server Classes)/l8/06232014.sql
@@ -0,0 +1,42 @@
+Function CheckObjectValidity ($server, $smolibrary)
+{
+    Add-Type -Path $smolibrary
+    $serv = New-Object Microsoft.SqlServer.Management.SMO.Server($server)
+    foreach ($db in $serv.Databases | Where-Object {$_.IsSystemObject -eq $false})
+    {
+        $d = $db.Name
+        foreach ($proc in $db.StoredProcedures | Where-Object {$_.IsSystemObject -eq $false})
+        {
+            $o = $proc.Name
+   ## Gets the schema for the procedure
+            $sc = $proc.Schema
+ 
+            $scon = New-Object System.Data.SqlClient.SqlConnection
+            $scon.ConnectionString = "SERVER=" + $server + ";DATABASE=" + $d + ";Integrated Security=true"
+            $trycmd = New-Object System.Data.SqlClient.SqlCommand
+            $catchcmd = New-Object System.Data.SqlClient.SqlCommand
+   ## We refresh the object with the schema.table
+            $trycmd.CommandText = "EXECUTE sys.sp_refreshsqlmodule '$sc.$o'"
+            $trycmd.Connection = $scon
+            $catchcmd.CommandText = "INSERT INTO Logging.dbo.ObjectValidityTable (DatabaseName,ObjectName) VALUES ('$d','$o')"
+            $catchcmd.Connection = $scon
+ 
+            try
+            {
+                $scon.Open()
+                $trycmd.ExecuteNonQuery()
+            }
+            catch
+            {
+                $catchcmd.ExecuteNonQuery()
+            }
+            finally
+            {
+                $scon.Close()
+                $scon.Dispose()
+            }
+        }
+    }
+}
+
+CheckObjectValidity -server "" -smolibrary ""
