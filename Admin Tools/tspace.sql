1fe566fc334ab08177f55a2d2766545e170775dd
Tim <tmmtsmith@gmail.com>
2014-07-19 04:46:56 -0500

    Update tspace.sql

--- Admin Tools/tspace.sql
+++ Admin Tools/tspace.sql
@@ -1,6 +1,6 @@
 -- Total and used space, as well as how many columns
 WITH OrderTheData AS(
-	SELECT t.NAME AS TableName,
+	SELECT t.name AS TableName,
 		p.rows AS TotalRows,
 		(SUM(a.total_pages) * 8)/(1024*1024) AS TotalSpaceGIG,
 		(SUM(a.used_pages) * 8)/(1024*1024) AS UsedSpaceGIG
@@ -10,7 +10,7 @@ WITH OrderTheData AS(
 		INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
 		INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
 	WHERE t.is_ms_shipped = 0
-	GROUP BY t.Name, s.Name, p.Rows
+	GROUP BY t.name, p.Rows
 ), ColumnCount AS(
 	SELECT DISTINCT TABLE_NAME TableName
 		, COUNT(COLUMN_NAME) ColumnCounts
