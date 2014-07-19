36e13d952d68cd1287c886b03647e16601e2c9fc
Tim <tmmtsmith@gmail.com>
2014-07-19 04:54:11 -0500

    Update tspace.sql

--- Admin Tools/tspace.sql
+++ Admin Tools/tspace.sql
@@ -1,26 +1,19 @@
 -- Total and used space, as well as how many columns
 WITH OrderTheData AS(
-	SELECT t.name AS TableName,
-		p.rows AS TotalRows,
-		(SUM(a.total_pages) * 8)/(1024*1024) AS TotalSpaceGIG,
-		(SUM(a.used_pages) * 8)/(1024*1024) AS UsedSpaceGIG
+	SELECT t.name AS TableName
+		, t.max_column_id_used ColumnCount
+		, p.rows AS TotalRows
+		, (SUM(a.total_pages) * 8)/(1024*1024) AS TotalSpaceGIG
+		, (SUM(a.used_pages) * 8)/(1024*1024) AS UsedSpaceGIG
 	FROM sys.tables t
-		INNER JOIN sys.schemas s ON s.schema_id = t.schema_id
 		INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
 		INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
 		INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
 	WHERE t.is_ms_shipped = 0
-	GROUP BY t.name, p.Rows
-), ColumnCount AS(
-	SELECT DISTINCT TABLE_NAME TableName
-		, COUNT(COLUMN_NAME) ColumnCounts
-	FROM INFORMATION_SCHEMA.COLUMNS
-	GROUP BY TABLE_NAME
+	GROUP BY t.name, t.max_column_id_used, p.Rows
 )
-SELECT o.*
-	, c.ColumnCounts
-FROM OrderTheData o
-	INNER JOIN ColumnCount c ON o.TableName = c.TableName
+SELECT *
+FROM OrderTheData 
 ORDER BY TotalSpaceGIG DESC, UsedSpaceGIG DESC
 
 
