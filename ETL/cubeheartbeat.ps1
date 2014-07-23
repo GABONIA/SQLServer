5fff9d374220f67a125043917f2d47aa6f3a82f2
Tim <tmmtsmith@gmail.com>
2014-07-23 13:40:07 -0500

    Update cubeheartbeat.ps1

--- ETL/cubeheartbeat.ps1
+++ ETL/cubeheartbeat.ps1
@@ -11,7 +11,7 @@ Function AnalysisServer_Heartbeat ($server, $database, $filepath, $emailto, $smt
         $heartbeat = Invoke-ASCmd -Server $server -Database $database -Query $mdx
         $error = "Either connection to $server failed, or the below query failed." + $nl + $nl + $mdx
 
-        if ((!$heartbeat) -or ($heartbeat -like "*not been processed*"))
+        if ((!$heartbeat) -or ($heartbeat -like "*not been processed*") -or ($heartbeat -like "*syntax*" -and $heartbeat -like "*incorrect*")
         {
             Send-MailMessage -To $emailto -From "Cube Alerts <cubealerts@ssas.com>" -Subject "Cube Heartbeat Failure" -Body $msg -SmtpServer $smtpserver    
         }
