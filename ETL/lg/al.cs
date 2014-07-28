250e85890f3fbf47e44d2ce0d86712084b648294
Tim <tmmtsmith@gmail.com>
2014-07-28 07:35:33 -0500

    Update alert

--- ETL/lg/al.cs
+++ ETL/lg/al.cs
@@ -2,29 +2,79 @@ public static class Alerts
 {
     public static bool TextAlert(string body, string textAlert = null)
     {
-        bool check = false;
         // If established:
         if (textAlert == null)
         {
-            textAlert = ""; // Default
+            textAlert = "";  // Default
         }
+
+        bool check = false;
         string smtp_sender = "";
         string from = "";
         SmtpClient smtpcl = new SmtpClient(smtp_sender);
 
-        try
+        if (body.Count() > 140)
         {
-            smtpcl.Send(from, textAlert, "", body);
-            check = true;
+            string b;
+            int start = 0;
+            float bodycount = (body.Count() / 140);
+            if (((body.Count() % 140) != 0))
+            {
+                for (int i = 1; i <= (bodycount + 1); i++)
+                {
+                    if (i == (bodycount + 1))
+                    {
+                        b = body.Substring(start, (body.Count() - start));
+                    }
+                    else
+                    {
+                        b = body.Substring(start, 140);
+                    }
+
+                    try
+                    {
+                        smtpcl.Send(from, textAlert, "", b);
+                        check = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Text alert failed.  Error: " + ex.ToString());
+                    }
+                    start = start + 140;
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= bodycount; i++)
+                {
+                    b = body.Substring(start, 140);
+                    try
+                    {
+                        smtpcl.Send(from, textAlert, "", b);
+                        check = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Text alert failed.  Error: " + ex.ToString());
+                    }
+                    start = start + 140;
+                }
+            }
         }
-        catch (Exception ex)
+        else
         {
-            Console.WriteLine("Text alert failed.  Error: " + ex.ToString());
+            try
+            {
+                smtpcl.Send(from, textAlert, "", body);
+                check = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Text alert failed.  Error: " + ex.ToString());
+            }
         }
-        finally 
-        {
-            smtpcl.Dispose();
-        }
+
+        smtpcl.Dispose();
         return check;
     }
     public static bool EmailAlert(string emailAlert)
