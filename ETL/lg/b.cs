5985dcfc21a54e64ededf34c61caa8b6c14e5090
Tim <tmmtsmith@gmail.com>
2014-07-30 06:55:02 -0500

    Update b.cs

--- ETL/lg/b.cs
+++ ETL/lg/b.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Timers;
+using System.Threading;
 using System.Threading.Tasks;
 // SQL Server SDK
 // Microsoft.SqlServer.ConnectionInfo.dll
 // Microsoft.SqlServer.SqlEnum.dll
 // Microsoft.SqlServer.Management.Sdk.Sfc.dll
 // Microsoft.SqlServer.Smo.dll
+using System.Net.Mail;
 using Microsoft.SqlServer.Management.Smo;
 
 
@@ -19,16 +23,115 @@ namespace DBA_ETL_Logger
     {
         static void Main(string[] args)
         {
-            // Tested: 9
-            // [x] UPST
-            // [] BK
-            // [x] Em
-            // [x] Txt
-            // [] Mult sv
-            // [x] CDB
             Console.ReadLine();
         }
     }
+
+
+    public static class Files
+    {
+        public static string FileAsString(string file)
+        {
+            string filetxt = "";
+            using (StreamReader read = new StreamReader(file, Encoding.UTF8))
+            {
+                filetxt = read.ReadToEnd();
+            }
+            return filetxt;
+        }
+    }
+
+
+    public static class Alerts
+    {
+        public static bool TextAlert(string body, string textAlert = null)
+        {
+            // If established:
+            if (textAlert == null)
+            {
+                // default
+                textAlert = "";
+            }
+
+            bool check = false;
+            // edit:
+            string smtp_sender = "";
+            // edit:
+            string from = "";
+            SmtpClient smtpcl = new SmtpClient(smtp_sender);
+
+            if (body.Count() > 140)
+            {
+                string b;
+                int start = 0;
+                float bodycount = (body.Count() / 140);
+                if (((body.Count() % 140) != 0))
+                {
+                    for (int i = 1; i <= (bodycount + 1); i++)
+                    {
+                        if (i == (bodycount + 1))
+                        {
+                            b = body.Substring(start, (body.Count() - start));
+                        }
+                        else
+                        {
+                            b = body.Substring(start, 140);
+                        }
+
+                        try
+                        {
+                            smtpcl.Send(from, textAlert, "", b);
+                            check = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Text alert failed.  Error: " + ex.ToString());
+                        }
+                        start = start + 140;
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i <= bodycount; i++)
+                    {
+                        b = body.Substring(start, 140);
+                        try
+                        {
+                            smtpcl.Send(from, textAlert, "", b);
+                            check = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Text alert failed.  Error: " + ex.ToString());
+                        }
+                        start = start + 140;
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    smtpcl.Send(from, textAlert, "", body);
+                    check = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Text alert failed.  Error: " + ex.ToString());
+                }
+            }
+
+            smtpcl.Dispose();
+            return check;
+        }
+        public static bool EmailAlert(string emailAlert)
+        {
+            bool check = false;
+
+            return check;
+        }
+    }
+
 
     public static class Connections
     {
@@ -45,6 +148,7 @@ namespace DBA_ETL_Logger
         }
     }
 
+
     public class SqlOperations
     {
         public static bool ExecuteCommand(string srv, string db, string cmdText)
@@ -52,6 +156,7 @@ namespace DBA_ETL_Logger
             using (var scon = Connections.Connect(srv, db))
             {
                 SqlCommand cmd = new SqlCommand(cmdText, scon);
+                cmd.CommandTimeout = 0;
                 bool b = false;
                 try
                 {
@@ -69,7 +174,6 @@ namespace DBA_ETL_Logger
                 }
                 return b;
             }
-            
         }
         public static bool BuildETLLogTable(string srv, string db, string tbName)
         {
@@ -113,7 +217,7 @@ namespace DBA_ETL_Logger
             }
             return ds;
         }
-        public static int IssueCheckpoints(string stringsrv)
+        public static string IssueCheckpoints(string stringsrv)
         {
             /*
             SELECT
@@ -128,7 +232,7 @@ namespace DBA_ETL_Logger
             FROM  fn_dblog(NULL, NULL) 
             WHERE Operation IN ('LOP_BEGIN_CKPT', 'LOP_END_CKPT')
             */
-            int x = 0;
+            string message = "";
             Server srv = Connections.ServerConnect(stringsrv);
             foreach (Database db in srv.Databases)
             {
@@ -138,16 +242,75 @@ namespace DBA_ETL_Logger
                     try
                     {
                         SqlOperations.ExecuteCommand(stringsrv, dbName, "CHECKPOINT;");
-                        Console.WriteLine("Checkpoint issued on " + dbName + ".");
+                        message = "Checkpoint issued on " + dbName + ".";
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine("Checkpoint failed on " + dbName + ".  " + ex.ToString());
-                        x++;
+                        message = "Checkpoint failed on " + dbName + "." + "\n" + "Exception: " + ex.ToString();
                     }
                 }
             }
+            return message;
+        }
+        public static int IssueCheckpoints_CertainDBs(string stringsrv, List<String> dbs)
+        {
+            int x = 0;
+            foreach (string db in dbs)
+            {
+                try
+                {
+                    SqlOperations.ExecuteCommand(stringsrv, db, "CHECKPOINT;");
+                    Console.WriteLine("Checkpoint issued on " + db + ".  Verify if this stops error log issues.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Checkpoint failed on " + db + ".  Exception: {0}.", ex);
+                    x++;
+                }
+            }
             return x;
+        }
+        public static string LargeUnCompressedTables(string stringsrv, double gig)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            foreach (Database db in srv.Databases)
+            {
+                if (db.IsSystemObject == false)
+                {
+                    foreach (Table tb in db.Tables)
+                    {
+                        if ((tb.DataSpaceUsed / (1024 * 1024)) > gig && ((tb.PhysicalPartitions[0].DataCompression).ToString() == "None"))
+                        {
+                            Console.WriteLine("Database: {0}, Table: {1}", db.Name, tb.Name);
+                        }
+                    }
+                }
+            }
+            return "Finished checking for large, uncompressed tables.";
+        }
+        public static string LargeUnCompressedIndexes(string stringsrv, double gig)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            foreach (Database db in srv.Databases)
+            {
+                if (db.IsSystemObject == false)
+                {
+                    foreach (Table tb in db.Tables)
+                    {
+                        foreach (Index idx in tb.Indexes)
+                        {
+                            if ((idx.SpaceUsed / (1024 * 1024)) > gig && ((idx.PhysicalPartitions[0].DataCompression).ToString() == "None"))
+                            {
+                                Console.WriteLine("Database: {0}, Table: {1}, Index: {2}", db.Name, tb.Name, idx.Name);
+                                //string rebuild = "ALTER INDEX " + idx.Name + " ON " + tb.Name + " REBUILD WITH (DATA_COMPRESSION = PAGE)";
+                                //Console.WriteLine(rebuild);
+                                //SqlOperations.ExecuteCommand(stringsrv,db.Name,rebuild);
+                            }
+                        }
+                    }
+                }
+            }
+            return "Finished checking for large, uncompressed indexes.";
         }
         public static int NonSystemDBsIssueCommand(string stringsrv, string cmdText)
         {
@@ -158,20 +321,254 @@ namespace DBA_ETL_Logger
                 if (db.IsSystemObject == false)
                 {
                     string dbName = db.Name;
-                    string srvName = srv.Name;
                     try
                     {
-                        SqlOperations.ExecuteCommand(srvName, dbName, cmdText);
+                        SqlOperations.ExecuteCommand(stringsrv, dbName, cmdText);
                         Console.WriteLine("Command succeeded on " + dbName + ".");
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine("Command failed on " + dbName + ".  Exception: {0}", ex);
+                        Console.WriteLine("Command failed on " + dbName + ".  Exception: {0}.", ex);
                         x++;
                     }
                 }
             }
             return x;
+        }
+        public static string ActiveTransactionDBs(string stringsrv)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            return srv.NumberOfLogFiles.ToString();
+        }
+        public static string ReadSQLLog_Recent(string stringsrv)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            DataTable dt = srv.ReadErrorLog();
+            foreach (DataRow row in dt.Rows)
+            {
+                DateTime colDate = Convert.ToDateTime(row["LogDate"]);
+                string message = Convert.ToString(row["Text"]) + "\n";
+
+                if ((colDate > DateTime.Now.AddMinutes(-60)) && (colDate < DateTime.Now))
+                {
+                    Console.WriteLine(message);
+                }
+            }
+            return "\n" + "Complete.";
+        }
+        public static string ReadSQLLog_LastTen(string stringsrv)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            DataTable dt = srv.ReadErrorLog();
+            foreach (DataRow row in dt.Rows)
+            {
+                DateTime colDate = Convert.ToDateTime(row["LogDate"]);
+                string message = Convert.ToString(row["Text"]) + "\n";
+
+                if ((colDate > DateTime.Now.AddMinutes(-10)) && (colDate < DateTime.Now))
+                {
+                    Console.WriteLine(message);
+                }
+            }
+            return "\n" + "Complete.";
+        }
+        public static List<String> ActiveTransactionSimpleDBs(string stringsrv)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            DataTable dt = srv.ReadErrorLog();
+            List<string> dbs = new List<string>();
+            string db;
+            foreach (DataRow row in dt.Rows)
+            {
+                DateTime colDate = Convert.ToDateTime(row["LogDate"]);
+                string message = Convert.ToString(row["Text"]) + "\n";
+
+                if ((colDate > DateTime.Now.AddMinutes(-30)) && (colDate < DateTime.Now))
+                {
+                    if (message.Contains("'ACTIVE_TRANSACTION'"))
+                    {
+                        db = message.Substring(message.IndexOf("'") + 1, (message.IndexOf("' is full") - (message.IndexOf("'") + 1)));
+                        if (!dbs.Contains(db))
+                        {
+                            dbs.Add(db);
+                        }
+                    }
+                }
+            }
+            return dbs;
+        }
+        public static string CheckDBErrors(string stringsrv)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            DataTable dt = srv.ReadErrorLog();
+            string status = "No CHECKDB errors for " + srv.Name + ".";
+            foreach (DataRow row in dt.Rows)
+            {
+                DateTime colDate = Convert.ToDateTime(row["LogDate"]);
+                string message = Convert.ToString(row["Text"]) + "\n";
+
+                if ((colDate > DateTime.Now.AddMinutes(-1440)) && (colDate < DateTime.Now))
+                {
+                    if ((message.Contains("DBCC CHECKDB")) && !message.Contains("found 0 errors"))
+                    {
+                        Alerts.TextAlert(message);
+                        status = "E.";
+                    }
+                }
+            }
+
+            if (status == "E")
+            {
+                status = "Review SQL Server error logs for server " + srv.Name;
+            }
+            Alerts.TextAlert(status);
+            return status;
+        }
+        public static string MultipleLogins (string stringsrv, int no)
+        {
+            int cnt = 0;
+            string status = "Logins cleared.";
+            Server srv = Connections.ServerConnect(stringsrv);
+            DataTable dt = srv.ReadErrorLog();
+            foreach (DataRow row in dt.Rows)
+            {
+                DateTime colDate = Convert.ToDateTime(row["LogDate"]);
+                string message = Convert.ToString(row["Text"]) + "\n";
+
+                if ((colDate > DateTime.Now.AddMinutes(-10)) && (colDate < DateTime.Now))
+                {
+                    if (message.Contains("Login failed"))
+                    {
+                        cnt++;
+                    }
+                }
+            }
+
+            if (cnt >= no)
+            {
+                Alerts.TextAlert(no.ToString() + " logins failed in the past ten minutes.");
+                status = no.ToString() + " logins failed in the past ten minutes.";
+            }
+            return status;
+        }
+        public static List<String> LowFreeSpaceDBs(string stringsrv, double lowspace)
+        {
+            Server srv = Connections.ServerConnect(stringsrv);
+            List<string> lowspacedbs = new List<string>();
+            decimal total;
+            decimal free;
+            foreach (Database db in srv.Databases)
+            {
+                total = Convert.ToDecimal(db.Size) /(1024);
+                free = Convert.ToDecimal(db.SpaceAvailable) / (Convert.ToDecimal(1024 * 1024));
+                if ((Convert.ToDouble(free/total)) < lowspace)
+                {
+                    Console.WriteLine(db.Name);
+                    lowspacedbs.Add(db.Name);
+                }
+            }
+            return lowspacedbs;
+        }
+        public static int ApplySQLFilesToServer(string stringsrv, string filepath, string onedb = null)
+        {
+            int x = 0;
+            Server srv = Connections.ServerConnect(stringsrv);
+            string[] sqlfiles = Directory.GetFiles(filepath, "*.sql");
+            
+            foreach (string fi in sqlfiles)
+            {
+                if (onedb == null)
+                {
+                    foreach (Database db in srv.Databases)
+                    {
+                        if (db.IsSystemObject == false)
+                        {
+                            string dbName = db.Name;
+                            try
+                            {
+                                SqlOperations.ExecuteCommand(stringsrv, dbName, Files.FileAsString(fi));
+                                Console.WriteLine("Command succeeded on " + dbName + ".");
+                                SqlOperations.ReturnConfirmation(stringsrv, dbName);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Command failed on " + dbName + ".  Exception: {0}.", ex);
+                                x++;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        SqlOperations.ExecuteCommand(stringsrv, onedb, Files.FileAsString(fi));
+                        Console.WriteLine("Command succeeded on " + onedb + ".");
+                        SqlOperations.ReturnConfirmation(stringsrv, onedb);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Command failed on " + onedb + ".  Exception: {0}.", ex);
+                        x = 1;
+                    }
+                }
+            }
+            return x;
+        }
+        public static int ApplySQLFilesToMultipleServers(string[] stringsrvs, string filepath, string onedb = null)
+        {
+            int x = 0;
+            foreach (string stringsrv in stringsrvs)
+            {
+                x = SqlOperations.ApplySQLFilesToServer(stringsrv, filepath, onedb);
+            }
+            return x;
+        }
+        public static string CountUncompressedTablesAndIndexes(string stringsrv, string dbName)
+        {
+            int idx_cnt = 0, tb_cnt = 0;
+            Server srv = Connections.ServerConnect(stringsrv);
+            Database db = srv.Databases[dbName];
+            if (db.IsSystemObject == false)
+            {
+                foreach (Table tb in db.Tables)
+                {
+                    if ((tb.PhysicalPartitions[0].DataCompression).ToString() == "None")
+                    {
+                        tb_cnt++;
+                    }
+
+                    foreach (Index idx in tb.Indexes)
+                    {
+                        if ((idx.PhysicalPartitions[0].DataCompression).ToString() == "None")
+                        {
+                            idx_cnt++;
+                        }
+                    }
+                }
+            }
+            string uncompressedtbsindxs = "\t" + "Uncompressed tables: " + tb_cnt.ToString() + "; uncompressed indexes: " + idx_cnt.ToString() + ".";
+            return uncompressedtbsindxs;
+        }
+        public static string ReturnConfirmation(string stringsrv, string db = null)
+        {
+            if (db == null)
+            {
+                db = "master";
+            }
+            string confirmation = "";
+            using (var scon = Connections.Connect(stringsrv, db))
+            {
+                SqlCommand retcon = new SqlCommand("SELECT CAST(DB_ID() AS VARCHAR(4)) + '_' + CAST(NEWID() AS VARCHAR(50)) + '_' + CONVERT(VARCHAR(8),GETDATE(),112)", scon);
+                SqlDataReader readSQL = retcon.ExecuteReader();
+                while (readSQL.Read())
+                {
+                    confirmation += readSQL.GetString(0);
+                }
+                scon.Close();
+                scon.Dispose();
+            }
+            return confirmation;
         }
     }
 }
