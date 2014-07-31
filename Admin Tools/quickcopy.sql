2b3be2d6a13c8fb1ec74ea494c098b9b0580439b
Tim <tmmtsmith@gmail.com>
2014-07-31 07:09:46 -0500

    DB1 Copy to DB2 Quick

---
+++ Admin Tools/quickcopy.sql
@@ -0,0 +1,24 @@
+IF OBJECT_ID('LoopTable') IS NOT NULL
+BEGIN
+	DROP TABLE LoopTable
+END
+
+SELECT ROW_NUMBER() OVER (ORDER BY name) ID 
+	, name
+INTO LoopTable
+FROM sys.tables
+
+DECLARE @b INT = 1, @m INT, @name VARCHAR(100), @sql NVARCHAR(MAX)
+SELECT @m = MAX(ID) FROM LoopTable
+
+WHILE @b <= @m
+BEGIN
+
+	SELECT @name = Name FROM LoopTable WHERE ID = @b
+
+	SET @sql = 'SELECT * INTO DatabaseTwo.dbo.' + @name + ' FROM ' + @name
+
+	EXEC sp_executesql @sql
+
+	SET @b = @b + 1
+END
