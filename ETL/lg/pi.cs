350bfebe30e549a3385eceb12f2c6e3254a83ba5
Tim <tmmtsmith@gmail.com>
2014-09-10 14:47:54 -0500

    init pi

---
+++ ETL/lg/pi.cs
@@ -0,0 +1,33 @@
+public class APIs
+{
+    public static int downloadFTPFiles(string ftpsite, string filter, string downloadpath)
+    {
+        string ftpfile = "";
+        int cnt = 0;
+        StreamReader read = null;
+        FtpWebRequest fareq = (FtpWebRequest)WebRequest.Create(ftpsite);
+        fareq.Method = WebRequestMethods.Ftp.DownloadFile;
+
+        fareq.Method = WebRequestMethods.Ftp.ListDirectory;
+        FtpWebResponse resp = (FtpWebResponse)fareq.GetResponse();
+
+        read = new StreamReader(resp.GetResponseStream());
+        string ln = read.ReadLine();
+        while (ln != null)
+        {
+            if (ln.Contains(filter) == true)
+            {
+                ftpfile = ftpsite + ln;
+                using (WebClient wbc = new WebClient())
+                {
+                    downloadpath = downloadpath + ln;
+                    wbc.DownloadFile(ftpfile, downloadpath);
+                }
+            }
+            ln = read.ReadLine();
+        }
+        read.Close();
+        read.Dispose();
+        return cnt;
+    }
+}
