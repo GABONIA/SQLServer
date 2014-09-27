9e208fb99a702029a539a0c6adb62f755e0c318e
Tim <tmmtsmith@gmail.com>
2014-09-26 19:07:48 -0500

    Update mtu_09262014.ps1

--- Teaching Examples (For SQL Server Classes)/presentations/mtu_09262014.ps1
+++ Teaching Examples (For SQL Server Classes)/presentations/mtu_09262014.ps1
@@ -24,7 +24,35 @@ Write-Host ($object/0).ToString()
 
 
 
-Window 3
+## Window 3
 $smo = "C:\Program Files\Microsoft SQL Server\110\SDK\Assemblies\Microsoft.SqlServer.Smo.dll"
 Add-Type -Path $smo
+
+
+
+## Window 4
+$server = "SERVER\INSTANCE"
+Add-Type -Path "C:\Program Files\Microsoft SQL Server\100\SDK\Assemblies\Microsoft.SqlServer.Smo.dll"
+$srv = New-Object Microsoft.SqlServer.Management.SMO.Server($server)
+
+foreach ($d in $srv.Databases | Where-Object {$_.IsSystemObject -eq $false})
+{
+    $d.Name
+}
+
+
+
+## Window 5
+Function Get-DBModel ($server)
+{
+    Add-Type -Path "C:\Program Files\Microsoft SQL Server\100\SDK\Assemblies\Microsoft.SqlServer.Smo.dll"
+    $srv = New-Object Microsoft.SqlServer.Management.SMO.Server($server)
+
+    foreach ($d in $srv.Databases | Where-Object {$_.IsSystemObject -eq $false})
+    {
+        $d.RecoveryModel
+    }
+}
+
+Get-DBModel -server "SERVER\INSTANCE"
 
