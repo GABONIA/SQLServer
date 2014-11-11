19ecb905de046bf5d468840e9e98bd56b4c8e9a6
Tim <tmmtsmith@gmail.com>
2014-11-11 08:42:16 -0800

    Size (gig)

---
+++ Admin Tools/Documentation/size.sql
@@ -0,0 +1,10 @@
+
+USE master
+GO
+
+SELECT DB_NAME(database_id)
+	, CASE WHEN physical_name LIKE '%.ldf' THEN name + ' (LOG)' ELSE name END AS LogicalName
+	, physical_name Location
+	, ((size*8)/(1024*1024)) GB
+FROM sys.master_files WITH(NOLOCK)
+ORDER BY size DESC
