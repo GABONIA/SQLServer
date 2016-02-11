b35ce2493f6987a9e72e1d696c7237e11d422036
Tim <tmmtsmith@gmail.com>
2016-02-11 08:01:33 -0600

    Update UnusedIndexes.sql

--- Tools/UnusedIndexes.sql
+++ Tools/UnusedIndexes.sql
@@ -1,10 +1,10 @@
 ---- Get unused indexes
 SELECT DISTINCT
-    OBJECT_NAME(s.[object_id]) AS ObjectName
-       , p.rows AS RowCount
-       , i.name AS IndexName
-       , (user_seeks + user_scans + user_lookups) AS TotalReads
-       , user_updates UserUpdates
+    OBJECT_NAME(s.[object_id]) AS [ObjectName]
+       , p.rows AS [RowCount]
+       , i.name AS [IndexName]
+       , (user_seeks + user_scans + user_lookups) AS [TotalReads]
+       , user_updates [UserUpdates]
 FROM sys.dm_db_index_usage_stats s
     INNER JOIN sys.indexes i ON i.[object_id] = s.[object_id] 
         AND i.index_id = s.index_id 
@@ -12,4 +12,4 @@ FROM sys.dm_db_index_usage_stats s
 WHERE OBJECTPROPERTY(s.[object_id],'IsUserTable') = 1
        AND s.database_id = DB_ID()
        AND i.name IS NOT NULL
-ORDER BY (user_seeks + user_scans + user_lookups) DESC
+ORDER BY (user_seeks + user_scans + user_lookups) ASC
