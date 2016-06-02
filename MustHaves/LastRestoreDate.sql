7c2b72b8dad6b1c1a1fe25c3c7efcfde2945c660
Tim <tmmtsmith@gmail.com>
2016-06-02 06:49:01 -0500

    Get last restore date by database

---
+++ MustHaves/LastRestoreDate.sql
@@ -0,0 +1,13 @@
+---- Order the restore date by database
+DECLARE @database VARCHAR(250) = 'master'
+SELECT 
+	t.name
+	, t.state_desc
+	, t.create_date
+	, tt.restore_date
+	--,t.*
+	--,tt.*
+FROM master.sys.databases t
+	LEFT JOIN msdb.dbo.restorehistory tt ON t.name = tt.destination_database_name
+WHERE t.name = @database
+ORDER BY tt.restore_date DESC
